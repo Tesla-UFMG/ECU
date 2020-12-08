bdd9623f59c94f74a7a2a61fe324a8e1c0f5619d
Renan Moreira <renanmoreira17@gmail.com>
2020-12-08 20:04:48 -0300

    Inicio de implementação de datalog

--- Core/Inc/CAN/CAN_IDs.h
+++ Core/Inc/CAN/CAN_IDs.h
@@ -11,15 +11,44 @@
 
 //INVERTER
 
-#define		ID_RIGHT_INVERTER 	0x301
-#define		ID_LEFT_INVERTER 	0x300
-#define		ID_COMM_FLAG		0x302		// flag de comunicacao ecu-inv ok
+#define	ID_RIGHT_INVERTER 	0x301
+#define	ID_LEFT_INVERTER 	0x300
+#define	ID_COMM_FLAG		0x302		// flag de comunicacao ecu-inv ok
 
 //----------
 //DATALOGGER
-
-
-
+//LEGENDA:  LABEL    	    	 ID  POS
+#define ID_ECU_TIMER        	{101, 0}
+#define ID_STEERING_WHEEL		{101, 1}
+#define ID_THROTTLE				{101, 2}
+#define ID_BRAKE				{101, 3}
+#define ID_MODE					{102, 0}
+#define ID_DISTANCE_ODOM		{102, 2}
+#define ID_TORQUE_R_MOTOR		{103, 0}
+#define ID_TORQUE_L_MOTOR		{103, 1}
+#define ID_REF_TORQUE_R_MOTOR	{103, 2}
+#define ID_REF_TORQUE_L_MOTOR	{103, 3}
+#define ID_REF_SPEED_R			{104, 0}
+#define ID_REF_SPEED_L			{104, 1}
+#define ID_REF_TORQUE_NEG		{104, 2}
+#define ID_REGEN_BRAKE_QUAN		{104, 3}
+#define ID_SPEED_FL				{105, 0}
+#define ID_SPEED_FR				{105, 1}
+#define ID_SPEED_RR				{105, 2}
+#define ID_SPEED_RL				{105, 3}
+#define ID_TORQUE_GAIN			{106, 0}
+#define ID_CURRENT_R_MOTOR		{106, 2}
+#define ID_CURRENT_L_MOTOR		{106, 3}
+#define ID_TEMPERATURE1_R		{107, 0}
+#define ID_TEMPERATURE2_R		{107, 1}
+#define ID_TEMPERATURE1_L		{107, 2}
+#define ID_TEMPERATURE2_L		{107, 3}
+#define ID_STATUS_DATALOG		{108, 1}
+#define ID_CURRENT_FLAG			{108, 3}
+#define ID_PANEL_DEBUG_1		{351, 0}
+#define ID_PANEL_DEBUG_2		{351, 1}
+#define ID_PANEL_DEBUG_3		{351, 2}
+#define ID_PANEL_DEBUG_4		{351, 3}
 //----------
 
 
--- Core/Src/throttle.c
+++ Core/Src/throttle.c
@@ -11,6 +11,7 @@ void throttle_read(void *argument) {
 	uint16_t APPS2;
 	uint16_t BSE;
 	uint16_t apps1_calc;
+	uint16_t aux_throttle_percent;
 	for (;;) {
 		APPS1 = ADC_DMA_buffer[APPS1_E];
 		APPS2 = ADC_DMA_buffer[APPS2_E];
@@ -18,44 +19,44 @@ void throttle_read(void *argument) {
 		apps1_calc = 0;
 
 		if (APPS2 < 260) {
-			throttle_percent = 0;
+			aux_throttle_percent = 0;
 		} else if (APPS2 >= 260 && APPS2 < 467) {
-			throttle_percent = 1.162 * APPS2 - 342.9;
+			aux_throttle_percent = 1.162 * APPS2 - 342.9;
 		} else if (APPS2 >= 467 && APPS2 < 1065) {
-			throttle_percent = 0.3344 * APPS2 + 43.8;
+			aux_throttle_percent = 0.3344 * APPS2 + 43.8;
 		} else if (APPS2 >= 1065 && APPS2 < 2253) {
-			throttle_percent = 0.1684 * APPS2 + 220.7;
+			aux_throttle_percent = 0.1684 * APPS2 + 220.7;
 		} else if (APPS2 >= 2253 && APPS2 < 3211) {
-			throttle_percent = 0.2087 * APPS2 + 129.9;
+			aux_throttle_percent = 0.2087 * APPS2 + 129.9;
 		} else if (APPS2 >= 3211 && APPS2 < 3720) {
-			throttle_percent = 0.6598 * APPS2 - 1319;
+			aux_throttle_percent = 0.6598 * APPS2 - 1319;
 		} else if (APPS2 >= 3720)
-			throttle_percent = 0;
+			aux_throttle_percent = 0;
 
 
-		if (throttle_percent >= 0 && throttle_percent < 200)
+		if (aux_throttle_percent >= 0 && aux_throttle_percent < 200)
 			apps1_calc = 2212;
-		else if (throttle_percent >= 200 && throttle_percent < 400)
+		else if (aux_throttle_percent >= 200 && aux_throttle_percent < 400)
 			apps1_calc = (uint16_t) (1.679 * acelerador + 1876);
-		else if (throttle_percent >= 400 && throttle_percent < 600)
+		else if (aux_throttle_percent >= 400 && aux_throttle_percent < 600)
 			apps1_calc = (uint16_t) (2.621 * acelerador + 1499);
-		else if (throttle_percent >= 600 && throttle_percent < 800)
+		else if (aux_throttle_percent >= 600 && aux_throttle_percent < 800)
 			apps1_calc = (uint16_t) (2.212 * acelerador + 1745);
-		else if (throttle_percent >= 800 && throttle_percent < 1135)
+		else if (aux_throttle_percent >= 800 && aux_throttle_percent < 1135)
 			apps1_calc = (uint16_t) (1.515 * acelerador + 2302);
 
 
 		if (APPS1 > 3900 || APPS1 < 1802.24)
-			throttle_percent = 0;
+			aux_throttle_percent = 0;
 		else if (APPS1 < apps1_calc * (1-APPS_PLAUSIBILITY_PERCENTAGE_TOLERANCE/100.0)
 			  || APPS1 > apps1_calc * (1+APPS_PLAUSIBILITY_PERCENTAGE_TOLERANCE/100.0))
-			throttle_percent = 0;
+			aux_throttle_percent = 0;
 
-		if (throttle_percent > 1000)
-			throttle_percent = 1000;
+		if (aux_throttle_percent > 1000)
+			aux_throttle_percent = 1000;
 
 
-		if (throttle_percent < 250 && BSE < 2200) {
+		if (aux_throttle_percent < 250 && BSE < 2200) {
 			//TODO: limpar flag de erro de APPS?
 //			*flag_error = 0;
 //			apps_t_flag = 1;
@@ -63,12 +64,13 @@ void throttle_read(void *argument) {
 
 		brake_status = BSE > 2200;
 
-		if (((throttle_percent > 300 && BSE > 2200) )) {
+		if (((aux_throttle_percent > 300 && BSE > 2200) )) {
 			//TODO: sinalizar evento de erro de APPS
-			throttle_percent = 0;
-			osEventFlagsSet(ECU_control_event_id, APPS_ERROR_FLAG);
+			aux_throttle_percent = 0;
+			//provavelmente não precisa sinalizar erro
+//			osEventFlagsSet(ECU_control_event_id, APPS_ERROR_FLAG);
 		}
-
+		throttle_percent = aux_throttle_percent;
 		//TODO: Averiguar duracao do delay
 		osDelay(100);
 

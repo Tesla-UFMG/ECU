3deca7feedbdc45e16ad98c9cef63ff831f5d1fe
FelTell <60394412+FelTell@users.noreply.github.com>
2021-07-13 12:44:22 -0300

    implementado função para zerar a velocidade das rodas

--- Core/Src/speed_calc.c
+++ Core/Src/speed_calc.c
@@ -29,6 +29,7 @@ extern TIM_HandleTypeDef htim2;
 extern osMessageQueueId_t q_speed_messageHandle;
 
 
+
 void speed_calc(void *argument) {
 	speed_message_t message;
 	speed_message_t last_messages[4];
@@ -36,8 +37,10 @@ void speed_calc(void *argument) {
 
 
 	const uint32_t	tim_freq  = HAL_RCC_GetPCLK1Freq(), 	//pega frequência do APB1, q está conectado ao tim2
-				   	tim_presc = htim2.Init.Prescaler+1,   	//pescaler do tim2
-					max_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MAX_SPEED;
+			tim_presc = htim2.Init.Prescaler+1,   	//prescaler do tim2
+			max_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MAX_SPEED,
+			min_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MIN_SPEED,
+			min_timeout = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * 1000 / MIN_SPEED;
 
 
 	uint32_t d_tim_count, speed;
@@ -48,42 +51,43 @@ void speed_calc(void *argument) {
 		brkpt();
 		#endif
 
-		switch(osMessageQueueGet(q_speed_messageHandle, &message, NULL,  1000)){ //espera até alguma mensagem chegar
+		switch(osMessageQueueGet(q_speed_messageHandle, &message, NULL,  min_timeout)){ //espera até alguma mensagem chegar ou timeout estourar
 
-
+		//caso a tarefa tenha sido chamada por timeout
 		case osErrorTimeout:
-			g_wheel_speed[FRONT_RIGHT] = 0;
-			g_wheel_speed[FRONT_LEFT] = 0;
-			g_wheel_speed[REAR_RIGHT] = 0;
-			g_wheel_speed[REAR_LEFT] = 0;
+			//zera a velocidade de todas as rodas
+			for(speed_pin_e i = FRONT_RIGHT; i <= REAR_LEFT; i++)
+				g_wheel_speed[i] = 0;
 			break;
 
+		//caso a tarefa tenha sido chamada pela queue
+		case osOK:
+			//verifica se alguma roda está a muito tempo sem receber interrupção, caso sim a velocidade dessa roda é zerada
+			for(speed_pin_e i = FRONT_RIGHT; i <= REAR_LEFT; i++)
+				if((message.tim_count - last_messages[i].tim_count) > min_count)
+					g_wheel_speed[i] = 0;
 
-		case osOK: ;
 
-		speed_pin_e wheel_pin = get_speed_pin(message.pin);
-		d_tim_count = message.tim_count - last_messages[wheel_pin].tim_count; //diferenca entre timestamp da mensagem atual e da anterior
+			speed_pin_e wheel_pin = get_speed_pin(message.pin);						//obtem de qual pino veio a interrupção
+			d_tim_count = message.tim_count - last_messages[wheel_pin].tim_count; 	//diferenca entre timestamp da mensagem atual e da anterior
 
+			if(d_tim_count < max_count) { //caso d_tim_count calcule uma velocidade maior do que é possível o valor será descartado
+				Error_Handler();
+				continue;
+			}
 
-		if(d_tim_count < max_count) { //caso d_tim_count calcule uma velocidade maior do que é possível o valor será descartado
-			Error_Handler();
-			continue;
+			speed = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)d_tim_count*tim_presc)); //calcula a velocidade
+			g_wheel_speed[wheel_pin] = speed; 		//seta velocidade especifica da roda recebida
+			last_messages[wheel_pin] = message; 	//guarda mensagem até a próxima interacão
+
+			uint16_t datalog_id = 	wheel_pin == FRONT_RIGHT ? 	ID_SPEED_FR :
+									wheel_pin == FRONT_LEFT  ? 	ID_SPEED_FL :
+									wheel_pin == REAR_RIGHT  ? 	ID_SPEED_RR :
+																ID_SPEED_RL;
+			log_data(datalog_id, (uint16_t) speed);
+		break;
 		}
-
-		speed = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)d_tim_count*tim_presc));
-		g_wheel_speed[wheel_pin] = speed; 	//seta velocidade especifica da roda recebida
-		last_messages[wheel_pin] = message; 	//guarda mensagem até a próxima interacão
-
-		uint16_t datalog_id = 	wheel_pin == FRONT_RIGHT ? 	ID_SPEED_FR :
-								wheel_pin == FRONT_LEFT  ? 	ID_SPEED_FL :
-								wheel_pin == REAR_RIGHT  ? 	ID_SPEED_RR :
-															ID_SPEED_RL;
-		log_data(datalog_id, (uint16_t) speed);
-			break;
 	}
-
-	//TODO: lógica para zerar velocidade de uma roda se n receber por certo período de tempo
-}
 }
 
 

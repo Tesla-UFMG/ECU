c15deda38149093f9e3de7cb2107d58e505198c7
Luiza Araújo <luizaaraujo1020@gmail.com>
2021-08-19 18:09:56 -0300

    controle lateral adicionado

---
+++ Core/Inc/DynamicControls/initializer_controls.h
@@ -0,0 +1,22 @@
+/*
+ * initializer_controls.h
+ *
+ *  Created on: 18 de ago de 2021
+ *      Author: Luiza
+ */
+
+#ifndef INC_DYNAMICCONTROLS_INITIALIZER_CONTROLS_H_
+#define INC_DYNAMICCONTROLS_INITIALIZER_CONTROLS_H_
+
+//vehicle parameters
+#define FRONT_DISTANCE 0.775                   // distance between the CG and the front axle
+#define REAR_DISTANCE 0.775                    // distance between the CG and the rear axle
+#define MASS 357                               // vehicle + pilot
+#define FRONT_CORNER_STIFF 50420.286           // cornering stiffness of the front axle
+#define REAR_CORNER_STIFF 50420.286            // cornering stiffness of the rear axle
+#define FRICTION_COEFFICIENT 1.3
+#define GRAVITY 9.81
+
+void init_controls();
+
+#endif /* INC_DYNAMICCONTROLS_INITIALIZER_CONTROLS_H_ */
---
+++ Core/Inc/DynamicControls/lateral_control.h
@@ -0,0 +1,17 @@
+/*
+ * lateral_control.h
+ *
+ *  Created on: 18 de ago de 2021
+ *      Author: Luiza
+ */
+
+#ifndef INC_DYNAMICCONTROLS_LATERAL_CONTROL_H_
+#define INC_DYNAMICCONTROLS_LATERAL_CONTROL_H_
+
+//defines PID
+#define KP 6.51864262048678
+#define TI 0.14843404179579
+
+#define LATERAL_DELAY 30
+
+#endif /* INC_DYNAMICCONTROLS_LATERAL_CONTROL_H_ */

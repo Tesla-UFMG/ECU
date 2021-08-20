4b428f9ec3c2ec05de3c3615d0bb85fc7b1be4c4
Luiza Araújo <luizaaraujo1020@gmail.com>
2021-08-20 15:32:13 -0300

    alterando rampa de torque e corrigindo erros

--- Core/Src/DynamicControls/lateral_control.c
+++ Core/Src/DynamicControls/lateral_control.c
@@ -5,24 +5,25 @@
  *      Author: Luiza
  */
 
-#include "lateral_control.h"
-#include "initializer_controls.h"
+#include "DynamicControls/lateral_control.h"
+#include "DynamicControls/initializer_controls.h"
+#include "DynamicControls/PID.h"
 #include "cmsis_os.h"
-#include "PID.h"
 
 extern volatile float g_wheel_speed[4];
 extern volatile uint16_t steering_wheel;
 extern volatile uint16_t gyro_yaw; // ainda nao existe
+extern const uint32_t ku;
+extern const uint32_t wheelbase;
+extern PID_t pid_lateral;
 
-uint32_t cg_speed, desired_yaw, max_yaw, setpoint;
-uint32_t ref_torque_decrease;
+lateral_t lateral_control() {
 
-
-uint32_t lateral_control() {
+	uint32_t cg_speed, desired_yaw, max_yaw, setpoint;
+	lateral_t ref_torque;
 
 	cg_speed = (g_wheel_speed[FRONT_RIGHT] + g_wheel_speed[FRONT_LEFT])/2;
-
-	//defines desired yaw (setpoint)
+	// defines desired yaw (setpoint)
 	desired_yaw = cg_speed * steering_wheel / (wheelbase + ku * cg_speed * cg_speed);
 	max_yaw = FRICTION_COEFFICIENT * GRAVITY / cg_speed;
 	if (desired_yaw > max_yaw)
@@ -30,8 +31,16 @@ uint32_t lateral_control() {
 	else
 		setpoint = desired_yaw;
 
+	// PID
 	PID_set_setpoint (&pid_lateral, setpoint);
-	ref_torque_decrease = PID_compute(&pid_lateral, gyro_yaw);
+	ref_torque.ref_decrease = PID_compute(&pid_lateral, gyro_yaw);
 
-	return ref_torque_decrease;
+	if (ref_torque.ref_decrease > 0)      // se o sinal for positivo, a reducao sera
+		ref_torque.wheel = REAR_RIGHT;    // na roda direita
+	else                                   // caso contrario, sera
+		ref_torque.wheel = REAR_LEFT;     // na roda esquerda
+
+	ref_torque.ref_decrease = abs(ref_torque.ref_decrease);
+
+	return ref_torque;
 }

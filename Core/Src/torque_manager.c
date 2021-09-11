fbcdd476033bd84a970c29839d0cca89c0cb6aa9
Luiza Araújo <luizaaraujo1020@gmail.com>
2021-09-11 10:19:16 -0300

    formatando pra boas praticas, corrigindo erros

--- Core/Src/torque_manager.c
+++ Core/Src/torque_manager.c
@@ -16,31 +16,6 @@ extern modos modo_selecionado;
 extern osMessageQueueId_t q_ref_torque_messageHandle;
 extern osMutexId_t m_state_parameter_mutexHandle;
 
-// Rampa vetorização
-void rampa_torque_lateral( lateral_t *ref_torque_decrease, uint32_t *ref_torque) {
-	static uint32_t ref_torque_ant[2] = {0};
-	ref_torque[R_MOTOR] = (modo_selecionado.torq_gain * throttle_percent) / 10;
-	ref_torque[L_MOTOR] = (modo_selecionado.torq_gain * throttle_percent) / 10;
-
-	if (ref_torque_decrease->ref_wheel == R_MOTOR)
-		ref_torque[R_MOTOR] -= (uint32_t)ref_torque_decrease->ref_decrease;
-	else
-		ref_torque[L_MOTOR] -= (uint32_t)ref_torque_decrease->ref_decrease;
-
-	for (int i=0;i<2;i++) {
-		if (ref_torque_ant[i] > TORQUE_INIT_LIMITE) {			           // verifica se a referencia
-			if (ref_torque[i] > ref_torque[i] + INC_TORQUE) {			   // ja passou do ponto de inflexao
-				ref_torque[i] = ref_torque_ant[i] + INC_TORQUE;			   // e aplica o incremento mais agressivo
-			}
-		} else {
-			if (ref_torque[i] > ref_torque_ant[i] + INC_TORQUE_INIT) {	   // caso contrario usa o
-				ref_torque[i] = ref_torque_ant[i] + INC_TORQUE_INIT;	   // incremento da primeira rampa
-			}
-		}
-		ref_torque_ant[i] = ref_torque[i];								   // aplica a referencia calculada
-	}
-}
-
 void torque_manager(void *argument) {
 
 	ref_torque_t ref_torque_message;
@@ -67,6 +42,7 @@ void torque_manager(void *argument) {
 			osDelayUntil(tick);
 
 			lateral_t lateral_control();
+			void rampa_torque_lateral(lateral_t *ref_torque_decrease, uint32_t *ref_torque);
 			lateral_t ref_torque_decrease = lateral_control();
 			uint32_t ref_torque_lateral[2] = {0};
 			rampa_torque_lateral(&ref_torque_decrease, ref_torque_lateral); // utilizar rampa_torque enquanto controle longitudinal nao estiver definido
@@ -113,4 +89,29 @@ uint32_t rampa_torque() {
 	ref_torque_ant = ref_torque;									// aplica a referencia calculada
 
 	return ref_torque;
+}
+
+// Rampa vetorização
+void rampa_torque_lateral(lateral_t *ref_torque_decrease, uint32_t *ref_torque) {
+	static uint32_t ref_torque_ant[2] = {0};
+	ref_torque[R_MOTOR] = (modo_selecionado.torq_gain * throttle_percent) / 10;
+	ref_torque[L_MOTOR] = (modo_selecionado.torq_gain * throttle_percent) / 10;
+
+	if (ref_torque_decrease->ref_wheel == R_MOTOR)
+		ref_torque[R_MOTOR] -= (uint32_t)ref_torque_decrease->ref_decrease;
+	else
+		ref_torque[L_MOTOR] -= (uint32_t)ref_torque_decrease->ref_decrease;
+
+	for (int i=0;i<2;i++) {
+		if (ref_torque_ant[i] > TORQUE_INIT_LIMITE) {			           // verifica se a referencia
+			if (ref_torque[i] > ref_torque_ant[i] + INC_TORQUE) {		   // ja passou do ponto de inflexao
+				ref_torque[i] = ref_torque_ant[i] + INC_TORQUE;			   // e aplica o incremento mais agressivo
+			}
+		} else {
+			if (ref_torque[i] > ref_torque_ant[i] + INC_TORQUE_INIT) {	   // caso contrario usa o
+				ref_torque[i] = ref_torque_ant[i] + INC_TORQUE_INIT;	   // incremento da primeira rampa
+			}
+		}
+		ref_torque_ant[i] = ref_torque[i];								   // aplica a referencia calculada
+	}
 }

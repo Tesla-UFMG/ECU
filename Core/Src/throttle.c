7d38d8930bb9d2b61f1c4f4342d9cb382784c554
Luiza Araújo <luizaaraujo1020@gmail.com>
2021-12-06 22:39:36 -0300

    testado e funcionando na white pill

--- Core/Src/throttle.c
+++ Core/Src/throttle.c
@@ -40,7 +40,7 @@ void throttle_read(void *argument) {
         APPS2 = ADC_DMA_buffer[APPS2_E];
         BSE   = ADC_DMA_buffer[BRAKE_E];
 
-        apps2_throttle_percent = calculate_apps2(APPS2);                      //calcula a porcentagem do pedal a partir do APPS2
+        apps2_throttle_percent = calculate_apps2(APPS2);                  //calcula a porcentagem do pedal a partir do APPS2
         apps1_calc = calculate_expected_apps1_from_apps2(apps2_throttle_percent);
 
         set_global_var_value(BRAKE_STATUS, (BSE > BRAKE_ACTIVE));
@@ -61,88 +61,38 @@ void throttle_read(void *argument) {
 
 // Calcula o valor do APPS2
 uint16_t calculate_apps2(uint16_t APPS2) {
-    // matrizes com valores do apps, obtidos no excel
-    uint16_t value[APPS_MATRIX_LENGTH] = {280, 470, 860, 1220, 1570, 1933, 2280, 2630, 3400};
-    uint16_t fix_mult[APPS_MATRIX_LENGTH] = {0, 1.5789, 0.2564, 0.2778, 0.2857, 0.2754, 0.2882, 0.2857, 0.2941};
-    int16_t fix_add[APPS_MATRIX_LENGTH] = {0, -442.1, 179.5, 161.1, 151.4, 167.5, 142.9, 148.6, 126.5};
-
+    uint16_t value[APPS_MATRIX_LENGTH] = {APPS2_REF};
+    float fix_mul[APPS_MATRIX_LENGTH] = {APPS2_FIX_MUL};
+    float fix_add[APPS_MATRIX_LENGTH] = {APPS2_FIX_ADD};
     uint16_t percentage = 0;
 
     for(int i=0; i<APPS_MATRIX_LENGTH; i++) {
         if(APPS2 < value[i]) {
-            percentage = fix_mult[i] * APPS2 + fix_add[i];
+            percentage = (fix_mul[i] * APPS2 + fix_add[i]);
             break;
         }
     }
-    /*
-    if(APPS2 < value[0])
-        percentage = fix_mult[0];
-    else if(APPS2 < apps2_value[1])
-        apps2_percentage = 1.5789 * APPS2 - 442.1;
-
-    if (APPS2 < 280)
-        apps2_percentage = 0;
-    else if (APPS2 >= 280 && APPS2 < 470)
-        apps2_percentage = 1.5789 * APPS2 - 442.1;
-    else if (APPS2 >= 470 && APPS2 < 860)
-        apps2_percentage = 0.2564 * APPS2 + 179.5;
-    else if (APPS2 >= 860 && APPS2 < 1220)
-        apps2_percentage = 0.2778 * APPS2 + 161.1;
-    else if (APPS2 >= 1220 && APPS2 < 1570)
-        apps2_percentage = 0.2857 * APPS2 + 151.4;
-    else if (APPS2 >= 1570 && APPS2 < 1933)
-        apps2_percentage = 0.2754 * APPS2 + 167.5;
-    else if (APPS2 >= 1933 && APPS2 < 2280)
-        apps2_percentage = 0.2882 * APPS2 + 142.9;
-    else if (APPS2 >= 2280 && APPS2 < 2630)
-        apps2_percentage = 0.2857 * APPS2 + 148.6;
-    else if (APPS2 >= 2630 && APPS2 < 3400)
-        apps2_percentage = 0.2941 * APPS2 + 126.5;
-    */
     if (percentage > 1000)
         percentage = 1000;
-
     return percentage;
 }
 
 // Calcula o valor teórico de APPS1 a partir do valor de APPS2
 uint16_t calculate_expected_apps1_from_apps2(uint16_t apps2_percentage) {
-    // matrizes com valores do apps, obtidos no excel
-    uint16_t value[APPS_MATRIX_LENGTH] = {200, 300, 400, 500, 600, 700, 800, 900, 1135};
-    uint16_t fix_mult[APPS_MATRIX_LENGTH] = {0, 1.75, 2.05, 1.75, 1.95, 1.85, 1.85, 1.95, 1.75};
-    uint16_t fix_add[APPS_MATRIX_LENGTH] = {2065, 1735, 1645, 1765, 1665, 1725, 1725, 1645, 1825};
+    uint16_t value[APPS_MATRIX_LENGTH] = {APPS1_REF};
+    float fix_mul[APPS_MATRIX_LENGTH] = {APPS1_FIX_MUL};
+    float fix_add[APPS_MATRIX_LENGTH] = {APPS1_FIX_ADD};
 
     if(apps2_percentage < 0 || apps2_percentage > value[8])
         return 0;
     else {
         for(int i=0; i<APPS_MATRIX_LENGTH; i++) {
             if(apps2_percentage < value[i]) {
-                return ((uint16_t)(fix_mult[i] * apps2_percentage + fix_add[i]));
+                return (fix_mul[i] * apps2_percentage + fix_add[i]);
             }
         }
     }
-    /*
-    if (apps2_percentage >= 0 && apps2_percentage < 200)
-        return (2065);
-    else if (apps2_percentage >= 200 && apps2_percentage < 300)
-        return ((uint16_t) (1.75 * apps2_percentage + 1735));
-    else if (apps2_percentage >= 300 && apps2_percentage < 400)
-        return ((uint16_t) (2.05 * apps2_percentage + 1645));
-    else if (apps2_percentage >= 400 && apps2_percentage < 500)
-        return ((uint16_t) (1.75 * apps2_percentage + 1765));
-    else if (apps2_percentage >= 500 && apps2_percentage < 600)
-        return ((uint16_t) (1.95 * apps2_percentage + 1665));
-    else if (apps2_percentage >= 600 && apps2_percentage < 700)
-        return ((uint16_t) (1.85 * apps2_percentage + 1725));
-    else if (apps2_percentage >= 700 && apps2_percentage < 800)
-        return ((uint16_t) (1.85 * apps2_percentage + 1725));
-    else if (apps2_percentage >= 800 && apps2_percentage < 900)
-        return ((uint16_t) (1.95 * apps2_percentage + 1645));
-    else if (apps2_percentage >= 900 && apps2_percentage < 1135)
-        return ((uint16_t) (1.75 * apps2_percentage + 1825));
-    else
-        return 0;
-    */
+    return 0;
 }
 
 

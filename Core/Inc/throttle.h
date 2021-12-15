abb24e3f3e55ab571c43df8578220fd16a48a357
Luiza Araújo <luizaaraujo1020@gmail.com>
2021-12-15 20:38:46 -0300

    melhorias

--- Core/Inc/throttle.h
+++ Core/Inc/throttle.h
@@ -14,18 +14,22 @@
 #define APPS_25_PERCENT 250
 #define APPS_05_PERCENT 50
 
-#define APPS_MATRIX_LENGTH 9
+// Valores abaixo obtidos durante a calibração do APPS
+// Explicações e mais detalhes disponíveis na planilha:
+// https://onedrive.live.com/view.aspx?resid=52B70A6379F6F8AB!115743&ithint=file%2cxlsx&authkey=!AJ8JGeSHTz3KdoQ
 
+#define APPS_MATRIX_LENGTH 9
+// APPS 1
 #define APPS1_REF 200, 300, 400, 500, 600, 700, 800, 900, 1135
 #define APPS1_FIX_MUL 0, 1.75, 2.05, 1.75, 1.95, 1.85, 1.85, 1.95, 1.75
 #define APPS1_FIX_ADD 2065, 1735, 1645, 1765, 1665, 1725, 1725, 1645, 1825
-
+#define APPS1_MAX 3700
+#define APPS1_MIN 1900
+// APPS 2
 #define APPS2_REF 280, 470, 860, 1220, 1570, 1933, 2280, 2630, 3400
 #define APPS2_FIX_MUL 0, 1.5789, 0.2564, 0.2778, 0.2857, 0.2754, 0.2882, 0.2857, 0.2941
 #define APPS2_FIX_ADD 0, -442.1, 179.5, 161.1, 151.4, 167.5, 142.9, 148.6, 126.5
-
-#define APPS2_MAX 3500
-#define APPS1_MAX 3700
-#define APPS1_MIN 1900
+#define APPS2_MAX_VALUE 3500
+#define APPS2_MAX_PERCENT 1000
 
 #endif /* INC_THROTTLE_H_ */
--- Core/Src/throttle.c
+++ Core/Src/throttle.c
@@ -8,15 +8,14 @@
 #include "throttle.h"
 #include "datalog_handler.h"
 #include "error_treatment.h"
+#include "math.h"
 #include "CMSIS_extra/global_variables_handler.h"
 #include "util.h"
 #include "global_variables.h"
 #include "global_instances.h"
 #include "cmsis_os.h"
 
-
-uint16_t calculate_expected_apps1_from_apps2(uint16_t apps2_percentage);
-uint16_t calculate_apps2(uint16_t APPS2);
+uint16_t throttle_calc(uint16_t APPS, uint16_t *value, float *fix_mul, float *fix_add, uint16_t MAX);
 bool is_there_APPS_error();
 bool is_there_BSE_error();
 bool is_there_SU_F_error();
@@ -40,8 +39,15 @@ void throttle_read(void *argument) {
         APPS2 = ADC_DMA_buffer[APPS2_E];
         BSE   = ADC_DMA_buffer[BRAKE_E];
 
-        apps2_throttle_percent = calculate_apps2(APPS2);                  //calcula a porcentagem do pedal a partir do APPS2
-        apps1_calc = calculate_expected_apps1_from_apps2(apps2_throttle_percent);
+        static uint16_t apps1_value[APPS_MATRIX_LENGTH] = {APPS1_REF};      // valores de referencia e parametros para
+        static float apps1_fix_mul[APPS_MATRIX_LENGTH] = {APPS1_FIX_MUL};   // o calculo da porcentagem.
+        static float apps1_fix_add[APPS_MATRIX_LENGTH] = {APPS1_FIX_ADD};   // disponíveis na planilha:
+        static uint16_t apps2_value[APPS_MATRIX_LENGTH] = {APPS2_REF};      // https://onedrive.live.com/view.aspx?resid=52B70A6379F6F8AB!115743&ithint=file%2cxlsx&authkey=!AJ8JGeSHTz3KdoQ
+        static float apps2_fix_mul[APPS_MATRIX_LENGTH] = {APPS2_FIX_MUL};
+        static float apps2_fix_add[APPS_MATRIX_LENGTH] = {APPS2_FIX_ADD};
+
+        apps2_throttle_percent = throttle_calc(APPS2,apps2_value,apps2_fix_mul,apps2_fix_add,APPS2_MAX_PERCENT);    //calcula a porcentagem do pedal a partir do APPS2
+        apps1_calc = throttle_calc(apps2_throttle_percent,apps1_value,apps1_fix_mul,apps1_fix_add,APPS1_MAX);       //calcula a porcentagem esperada do APPS1 a partir da porcentagem calculada do APPS2
 
         set_global_var_value(BRAKE_STATUS, (BSE > BRAKE_ACTIVE));
         set_global_var_value(THROTTLE_STATUS, (apps2_throttle_percent > 0));
@@ -59,47 +65,22 @@ void throttle_read(void *argument) {
     }
 }
 
-// Calcula o valor do APPS2
-uint16_t calculate_apps2(uint16_t APPS2) {
-    uint16_t value[APPS_MATRIX_LENGTH] = {APPS2_REF};
-    float fix_mul[APPS_MATRIX_LENGTH] = {APPS2_FIX_MUL};
-    float fix_add[APPS_MATRIX_LENGTH] = {APPS2_FIX_ADD};
-    uint16_t percentage = 0;
-
-    for(int i=0; i<APPS_MATRIX_LENGTH; i++) {
-        if(APPS2 < value[i]) {
-            percentage = (fix_mul[i] * APPS2 + fix_add[i]);
-            break;
-        }
-    }
-    if (percentage > 1000)
-        percentage = 1000;
-    return percentage;
-}
-
-// Calcula o valor teórico de APPS1 a partir do valor de APPS2
-uint16_t calculate_expected_apps1_from_apps2(uint16_t apps2_percentage) {
-    uint16_t value[APPS_MATRIX_LENGTH] = {APPS1_REF};
-    float fix_mul[APPS_MATRIX_LENGTH] = {APPS1_FIX_MUL};
-    float fix_add[APPS_MATRIX_LENGTH] = {APPS1_FIX_ADD};
-
-    if(apps2_percentage < 0 || apps2_percentage > value[8])
+uint16_t throttle_calc(uint16_t APPS, uint16_t *value, float *fix_mul, float *fix_add, uint16_t MAX) {
+    if(APPS < 0)
         return 0;
-    else {
-        for(int i=0; i<APPS_MATRIX_LENGTH; i++) {
-            if(apps2_percentage < value[i]) {
-                return (fix_mul[i] * apps2_percentage + fix_add[i]);
-            }
+    else                                                                    // compara o valor do APPS com as faixas de acionamento
+        for (int i=0; i<APPS_MATRIX_LENGTH; i++) {                          // para escolher quais parametros utilizar durante o
+            if(APPS < value[i]) {                                           // calculo da porcentagem.
+                return fmin(MAX,(fix_mul[i] * APPS + fix_add[i]));          // Se for maior que o maximo permitido, retorna maximo.
         }
     }
-    return 0;
+    return MAX;
 }
 
-
-bool is_there_APPS_error() {        //Regulamento: T.4.2 (2021)
-    if (    APPS2 >= APPS2_MAX      //Se o valor de APPS2 for acima do seu máximo
-         || APPS1 < APPS1_MIN       //Se o valor de APPS1 for abaixo do seu mínimo
-         || APPS1 > APPS1_MAX       //Se o valor de APPS1 for acima do seu máximo
+bool is_there_APPS_error() {            //Regulamento: T.4.2 (2021)
+    if (    APPS2 >= APPS2_MAX_VALUE    //Se o valor de APPS2 for acima do seu máximo
+         || APPS1 < APPS1_MIN           //Se o valor de APPS1 for abaixo do seu mínimo
+         || APPS1 > APPS1_MAX           //Se o valor de APPS1 for acima do seu máximo
          || APPS1 < apps1_calc * (1-APPS_PLAUSIBILITY_PERCENTAGE_TOLERANCE/100.0)   //verifica se APPS1 está abaixo do valor teórico de APPS1, considerando a tolerância
          || APPS1 > apps1_calc * (1+APPS_PLAUSIBILITY_PERCENTAGE_TOLERANCE/100.0))  //verifica se APPS1 está acima do valor teórico de APPS1, considerando a tolerância
         return true;

a99dc98abc2e3c5677e73c671e2a55d3ba2101aa
FelTell <60394412+FelTell@users.noreply.github.com>
2021-12-15 23:32:19 -0300

    melhorando identação e comentários

--- Core/Src/speed_calc.c
+++ Core/Src/speed_calc.c
@@ -35,14 +35,14 @@ void speed_calc(void *argument) {
     memset(&last_messages, 0, sizeof(speed_message_t)*4);   //inicializa com 0 buffer de ultimas mensagens
 
 
-    const uint32_t  tim_freq  = get_tim2_freq(),     //pega frequência do APB1, q está conectado ao tim2
-            tim_presc = htim2.Init.Prescaler+1,             //prescaler do tim2
-            //valor em tempo do timer2 da velocidade máxima a ser calculada
-            max_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MAX_SPEED,
-            //valor em tempo do timer2 da velocidade mínima a ser calculada
-            min_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MIN_SPEED,
-            //valor em tempo do timersys da velocidade mínima a ser calculada
-            min_timeout = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * 1000 / MIN_SPEED;
+    const uint32_t  tim_freq  = get_tim2_freq(),    //pega frequência que está conectada ao tim2
+    tim_presc = htim2.Init.Prescaler+1,             //prescaler do tim2
+    //valor em tempo do timer2 da velocidade máxima a ser calculada
+    max_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MAX_SPEED,
+    //valor em tempo do timer2 da velocidade mínima a ser calculada
+    min_count = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * ((float)tim_freq/((float)tim_presc)) / MIN_SPEED,
+    //valor em tempo do timersys da velocidade mínima a ser calculada
+    min_timeout = (10*3.6*2*M_PI * WHEEL_RADIUS / SPEED_SENSOR_TEETH_QUAN) * 1000 / MIN_SPEED;
 
 
     uint32_t d_tim_count, speed;
@@ -106,6 +106,7 @@ void reset_speed_single(speed_message_t* message, speed_message_t* last_messages
     set_global_var(WHEEL_SPEEDS, &wheel_speeds);
 }
 
+//obtem a frequencia do tim2 a partir APB1, considerando que ele pode ter um prescaler que dobra a frequencia
 uint32_t get_tim2_freq() {
     if (RCC->D2CFGR & RCC_D2CFGR_D2PPRE1)   // Get PCLK1 prescaler
         return 2*HAL_RCC_GetPCLK1Freq();    // PCLK1 prescaler different from 1 => TIMCLK = 2 * PCLK1

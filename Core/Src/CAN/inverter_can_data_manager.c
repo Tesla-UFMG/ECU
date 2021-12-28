7d0ea903b011272d94a83a9939deef3c16e86f2c
Renan Moreira <renanmoreira17@gmail.com>
2021-12-27 23:05:26 -0300

    Adicionando X-macro removida

--- Core/Src/CAN/inverter_can_data_manager.c
+++ Core/Src/CAN/inverter_can_data_manager.c
@@ -23,8 +23,9 @@ uint16_t get_value(can_vars_e var_name)
 can_vars_e get_var_name_from_id_and_pos(int id, int pos)
 {
     #define ENTRY(a,b,c) \
-    if (id == (b) && pos == (c)) return a; else
-    
+    if (id == (b) && pos == (c)) return a; \
+    else
+    VARIABLES;
     #undef ENTRY
     return -1;
 }

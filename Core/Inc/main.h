1dc89b74a57cb0951440740a5d1249c87bc6033c
FelTell <60394412+FelTell@users.noreply.github.com>
2022-02-13 16:04:32 -0300

    atualização do .ioc pelo cubeide

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -115,5 +115,3 @@ void Error_Handler(void);
 #endif
 
 #endif /* __MAIN_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -72,5 +72,3 @@ void EXTI15_10_IRQHandler(void);
 #endif
 
 #endif /* __STM32H7xx_IT_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

de7a51198fb1e26da8ba5addd6b838679fca62c4
dmrohrs <dmrohrs@gmail.com>
2022-04-22 12:57:36 -0300

    mais algumas mudancas

--- Core/Inc/CAN/CAN_IDs.h
+++ Core/Inc/CAN/CAN_IDs.h
@@ -10,10 +10,67 @@
 
 #include "stdint.h"
 
+//----------
+// DATALOGGER
+// LEGENDA:  LABEL               INTERNAL_ID
+// 0 E RESERVADO PARA LACUNAS NO PACOTE
 
 //Variable name, Message Id, Message word
 #define CAN_LIST \
-    CAN_LIST_DATA(ID_DISTANCE_T_ODOM, 102,1)
+    CAN_LIST_DATA(RESERVADO, 0, 0)                                          \
+    CAN_LIST_DATA(ID_ECU_TIMER, 101, 0)                                     \
+    CAN_LIST_DATA(ID_STEERING_WHEEL, 101, 1)                                \
+    CAN_LIST_DATA(ID_THROTTLE, 101, 2)                                      \
+    CAN_LIST_DATA(ID_BRAKE, 101, 3)                                         \
+    CAN_LIST_DATA(ID_MODE, 102, 0)                                          \
+    CAN_LIST_DATA(ID_REGEN_BRAKE_STATE, 102, 1)                             \
+    CAN_LIST_DATA(ID_DISTANCE_T_ODOM, 102, 2)                               \
+    CAN_LIST_DATA(ID_DISTANCE_P_ODOM, 102, 3)                               \
+    CAN_LIST_DATA(ID_TORQUE_R_MOTOR, 103, 0)                                \
+    CAN_LIST_DATA(ID_TORQUE_L_MOTOR, 103, 1)                                \
+    CAN_LIST_DATA(ID_REF_TORQUE_R_MOTOR, 103, 2)                            \
+    CAN_LIST_DATA(ID_REF_TORQUE_L_MOTOR, 103, 3)                            \
+    CAN_LIST_DATA(ID_REF_SPEED_R, 104, 0)                                   \
+    CAN_LIST_DATA(ID_REF_SPEED_L, 104, 1)                                   \
+    CAN_LIST_DATA(ID_CONTROL_EVENT_FLAG_1, 104, 2)                          \
+    CAN_LIST_DATA(ID_CONTROL_EVENT_FLAG_2, 104, 3)                          \
+    CAN_LIST_DATA(ID_SPEED_FL, 105, 0)                                      \
+    CAN_LIST_DATA(ID_SPEED_FR, 105, 1)                                      \
+    CAN_LIST_DATA(ID_SPEED_RR, 105, 2)                                      \
+    CAN_LIST_DATA(ID_SPEED_RL, 105, 3)                                      \
+    CAN_LIST_DATA(ID_TORQUE_GAIN, 106, 0)                                   \
+    CAN_LIST_DATA(ID_CURRENT_R_MOTOR, 106, 2)                               \
+    CAN_LIST_DATA(ID_CURRENT_L_MOTOR, 106, 3)                               \
+    CAN_LIST_DATA(ID_TEMPERATURE1_R, 107, 0)                                \
+    CAN_LIST_DATA(ID_TEMPERATURE2_R, 107, 1)                                \
+    CAN_LIST_DATA(ID_TEMPERATURE1_L, 107, 2)                                \
+    CAN_LIST_DATA(ID_TEMPERATURE2_L, 107, 3)                                \
+    CAN_LIST_DATA(ID_STATUS_DATALOG, 108, 1)                                \
+    CAN_LIST_DATA(ID_SPEED_L_MOTOR, 109, 0)                                 \
+    CAN_LIST_DATA(ID_SPEED_R_MOTOR, 109, 1)                                 \
+    CAN_LIST_DATA(ID_POWER_L_MOTOR, 109, 2)                                 \
+    CAN_LIST_DATA(ID_POWER_R_MOTOR, 109, 3)                                 \
+    CAN_LIST_DATA(ID_ENERGY_L_MOTOR, 110, 0)                                \
+    CAN_LIST_DATA(ID_ENERGY_R_MOTOR, 110, 1)                                \
+    CAN_LIST_DATA(ID_OVERLOAD_L_MOTOR, 110, 2)                              \
+    CAN_LIST_DATA(ID_OVERLOAD_R_MOTOR, 110, 3)                              \
+    CAN_LIST_DATA(ID_LOST_MSG_L_MOTOR, 111, 0)                              \
+    CAN_LIST_DATA(ID_LOST_MSG_R_MOTOR, 111, 1)                              \
+    CAN_LIST_DATA(ID_BUS_OFF_L_MOTOR, 111, 2)                               \
+    CAN_LIST_DATA(ID_BUS_OFF_R_MOTOR, 111, 3)                               \
+    CAN_LIST_DATA(ID_CAN_STATE_L_MOTOR, 112, 0)                             \
+    CAN_LIST_DATA(ID_CAN_STATE_R_MOTOR, 112, 1)                             \
+    CAN_LIST_DATA(ID_INV_STATE_L_MOTOR, 112, 2)                             \
+    CAN_LIST_DATA(ID_INV_STATE_R_MOTOR, 112, 3)                             \
+    CAN_LIST_DATA(ID_FAILURE_L_MOTOR, 113, 0)                               \
+    CAN_LIST_DATA(ID_FAILURE_R_MOTOR, 113, 1)                               \
+    CAN_LIST_DATA(ID_ALARM_L_MOTOR, 113, 2)                                 \
+    CAN_LIST_DATA(ID_ALARM_R_MOTOR, 113, 3)                                 \
+    CAN_LIST_DATA(ID_CURRENT_FLAG, 108, 3)                                  \
+    CAN_LIST_DATA(ID_PANEL_DEBUG_1,351, 0)                                  \
+    CAN_LIST_DATA(ID_PANEL_DEBUG_2, 351, 1)                                 \
+    CAN_LIST_DATA(ID_PANEL_DEBUG_3, 351, 2)                                 \
+    CAN_LIST_DATA(ID_PANEL_DEBUG_4, 351, 3)
 
 typedef enum
 {
@@ -30,72 +87,10 @@ typedef struct {
     uint8_t pos;
 } CAN_ID_t;
 
-
-
-
 // INVERTER
-
 #define ID_RIGHT_INVERTER 0x301
 #define ID_LEFT_INVERTER  0x300
 #define ID_COMM_FLAG      0x302 // flag de comunicacao ecu-inv ok
-
-//----------
-// DATALOGGER
-// LEGENDA:  LABEL    	    	 INTERNAL_ID
-// 0 E RESERVADO PARA LACUNAS NO PACOTE
-#define ID_ECU_TIMER            1
-#define ID_STEERING_WHEEL       2
-#define ID_THROTTLE             3
-#define ID_BRAKE                4
-#define ID_MODE                 5
-#define ID_DISTANCE_ODOM        6
-#define ID_TORQUE_R_MOTOR       7
-#define ID_TORQUE_L_MOTOR       8
-#define ID_REF_TORQUE_R_MOTOR   9
-#define ID_REF_TORQUE_L_MOTOR   10
-#define ID_REF_SPEED_R          11
-#define ID_REF_SPEED_L          12
-#define ID_CONTROL_EVENT_FLAG_1 13
-#define ID_CONTROL_EVENT_FLAG_2 14
-#define ID_SPEED_FL             15
-#define ID_SPEED_FR             16
-#define ID_SPEED_RR             17
-#define ID_SPEED_RL             18
-#define ID_TORQUE_GAIN          19
-#define ID_CURRENT_R_MOTOR      20
-#define ID_CURRENT_L_MOTOR      21
-#define ID_TEMPERATURE1_R       22
-#define ID_TEMPERATURE2_R       23
-#define ID_TEMPERATURE1_L       24
-#define ID_TEMPERATURE2_L       25
-#define ID_STATUS_DATALOG       26
-#define ID_CURRENT_FLAG         27
-#define ID_PANEL_DEBUG_1        28
-#define ID_PANEL_DEBUG_2        29
-#define ID_PANEL_DEBUG_3        30
-#define ID_PANEL_DEBUG_4        31
-#define ID_REGEN_BRAKE_STATE    32
-#define ID_SPEED_L_MOTOR        33
-#define ID_SPEED_R_MOTOR        34
-#define ID_POWER_L_MOTOR        35
-#define ID_POWER_R_MOTOR        36
-#define ID_ENERGY_L_MOTOR       37
-#define ID_ENERGY_R_MOTOR       38
-#define ID_OVERLOAD_L_MOTOR     39
-#define ID_OVERLOAD_R_MOTOR     40
-#define ID_LOST_MSG_L_MOTOR     41
-#define ID_LOST_MSG_R_MOTOR     42
-#define ID_BUS_OFF_L_MOTOR      43
-#define ID_BUS_OFF_R_MOTOR      44
-#define ID_CAN_STATE_L_MOTOR    45
-#define ID_CAN_STATE_R_MOTOR    46
-#define ID_INV_STATE_L_MOTOR    47
-#define ID_INV_STATE_R_MOTOR    48
-#define ID_FAILURE_L_MOTOR      49
-#define ID_FAILURE_R_MOTOR      50
-#define ID_ALARM_L_MOTOR        51
-#define ID_ALARM_R_MOTOR        52
-
 
 // TODO(renanmoreira): IMPLEMENT X-MACROS FOR CAN_ID'S
 #define ECU_CAN_INITIAL_ID        100
@@ -104,112 +99,6 @@ typedef struct {
 #define ECU_CAN_FIRST_DEBUG_ID    351
 #define ECU_CAN_LAST_DEBUG_ID     351
 
-#define INTERNAL_ID_ECU_TIMER                                                            \
-    { 101, 0 }
-#define INTERNAL_ID_STEERING_WHEEL                                                       \
-    { 101, 1 }
-#define INTERNAL_ID_THROTTLE                                                             \
-    { 101, 2 }
-#define INTERNAL_ID_BRAKE                                                                \
-    { 101, 3 }
-#define INTERNAL_ID_MODE                                                                 \
-    { 102, 0 }
-#define INTERNAL_ID_DISTANCE_ODOM                                                        \
-    { 102, 2 }
-#define INTERNAL_ID_TORQUE_R_MOTOR                                                       \
-    { 103, 0 }
-#define INTERNAL_ID_TORQUE_L_MOTOR                                                       \
-    { 103, 1 }
-#define INTERNAL_ID_REF_TORQUE_R_MOTOR                                                   \
-    { 103, 2 }
-#define INTERNAL_ID_REF_TORQUE_L_MOTOR                                                   \
-    { 103, 3 }
-#define INTERNAL_ID_REF_SPEED_R_MOTOR                                                    \
-    { 104, 0 }
-#define INTERNAL_ID_REF_SPEED_L_MOTOR                                                    \
-    { 104, 1 }
-#define INTERNAL_ID_CONTROL_EVENT_FLAG_1                                                 \
-    { 104, 2 }
-#define INTERNAL_ID_CONTROL_EVENT_FLAG_2                                                 \
-    { 104, 3 }
-#define INTERNAL_ID_SPEED_FL                                                             \
-    { 105, 0 }
-#define INTERNAL_ID_SPEED_FR                                                             \
-    { 105, 1 }
-#define INTERNAL_ID_SPEED_RR                                                             \
-    { 105, 2 }
-#define INTERNAL_ID_SPEED_RL                                                             \
-    { 105, 3 }
-#define INTERNAL_ID_TORQUE_GAIN                                                          \
-    { 106, 0 }
-#define INTERNAL_ID_CURRENT_R_MOTOR                                                      \
-    { 106, 2 }
-#define INTERNAL_ID_CURRENT_L_MOTOR                                                      \
-    { 106, 3 }
-#define INTERNAL_ID_TEMPERATURE1_R                                                       \
-    { 107, 0 }
-#define INTERNAL_ID_TEMPERATURE2_R                                                       \
-    { 107, 1 }
-#define INTERNAL_ID_TEMPERATURE1_L                                                       \
-    { 107, 2 }
-#define INTERNAL_ID_TEMPERATURE2_L                                                       \
-    { 107, 3 }
-#define INTERNAL_ID_STATUS_DATALOG                                                       \
-    { 108, 1 }
-#define INTERNAL_ID_CURRENT_FLAG                                                         \
-    { 108, 3 }
-#define INTERNAL_ID_PANEL_DEBUG_1                                                        \
-    { 351, 0 }
-#define INTERNAL_ID_PANEL_DEBUG_2                                                        \
-    { 351, 1 }
-#define INTERNAL_ID_PANEL_DEBUG_3                                                        \
-    { 351, 2 }
-#define INTERNAL_ID_PANEL_DEBUG_4                                                        \
-    { 351, 3 }
-#define INTERNAL_ID_REGEN_BRAKE_STATE                                                    \
-    { 102, 1 }
-#define INTERNAL_ID_SPEED_L_MOTOR                                                        \
-    { 109, 0 }
-#define INTERNAL_ID_SPEED_R_MOTOR                                                        \
-    { 109, 1 }
-#define INTERNAL_ID_POWER_L_MOTOR                                                        \
-    { 109, 2 }
-#define INTERNAL_ID_POWER_R_MOTOR                                                        \
-    { 109, 3 }
-#define INTERNAL_ID_ENERGY_L_MOTOR                                                       \
-    { 110, 0 }
-#define INTERNAL_ID_ENERGY_R_MOTOR                                                       \
-    { 110, 1 }
-#define INTERNAL_ID_OVERLOAD_L_MOTOR                                                     \
-    { 110, 2 }
-#define INTERNAL_ID_OVERLOAD_R_MOTOR                                                     \
-    { 110, 3 }
-#define INTERNAL_ID_LOST_MSG_L_MOTOR                                                     \
-    { 111, 0 }
-#define INTERNAL_ID_LOST_MSG_R_MOTOR                                                     \
-    { 111, 1 }
-#define INTERNAL_ID_BUS_OFF_L_MOTOR                                                      \
-    { 111, 2 }
-#define INTERNAL_ID_BUS_OFF_R_MOTOR                                                      \
-    { 111, 3 }
-#define INTERNAL_ID_CAN_STATE_L_MOTOR                                                    \
-    { 112, 0 }
-#define INTERNAL_ID_CAN_STATE_R_MOTOR                                                    \
-    { 112, 1 }
-#define INTERNAL_ID_INV_STATE_L_MOTOR                                                    \
-    { 112, 2 }
-#define INTERNAL_ID_INV_STATE_R_MOTOR                                                    \
-    { 112, 3 }
-#define INTERNAL_ID_FAILURE_L_MOTOR                                                      \
-    { 113, 0 }
-#define INTERNAL_ID_FAILURE_R_MOTOR                                                      \
-    { 113, 1 }
-#define INTERNAL_ID_ALARM_L_MOTOR                                                        \
-    { 113, 2 }
-#define INTERNAL_ID_ALARM_R_MOTOR                                                        \
-    { 113, 3 }
-
-//----------
 
 uint16_t convert_id_pos_to_index(uint16_t id, uint16_t pos);
 CAN_ID_t get_CAN_ID_from_internal(uint16_t internal);
--- Core/Src/CAN/CAN_IDs.c
+++ Core/Src/CAN/CAN_IDs.c
@@ -8,12 +8,27 @@
 
 CAN_ID_t CAN_ID_map[CAN_ID_QUAN];
 // TODO(renanmoreira): talvez aumentar capacidade se precisar de mais ids de debug
-uint16_t
-    CAN_ID_POS_INTERNAL_ID_MAP[(ECU_CAN_LAST_POPULATED_ID - ECU_CAN_INITIAL_ID + 1) * 4
-                               + 8];
 
-uint16_t convert_id_pos_to_index(uint16_t id, uint16_t pos) {
-    return (id - ECU_CAN_INITIAL_ID) * 4 + pos;
+uint16_t CAN_ID_POS_INTERNAL_ID_MAP[(ECU_CAN_LAST_POPULATED_ID - ECU_CAN_INITIAL_ID + 1) * 4 + 8];
+
+void initialize_CAN_IDs()
+{
+    #define CAN_LIST_DATA (var_name, msg_id, msg_wrd)\
+        CAN_ID_map[##var_name].id = ##msg_id;  \
+        CAN_ID_map[##var_name].pos = ##msg_wrd; \
+        CAN_LIST
+    #undef CAN_LIST_DATA
+}
+
+uint16_t convert_id_pos_to_index(uint16_t id, uint16_t pos)
+{
+    #define CAN_LIST_DATA(var_name, msg_id, msg_wrd)\
+        if ( (msg_id == id) && (msg_wrd == pos) )   \
+        {                                           \
+            return var_name;                        \
+        }                                           \
+        CAN_LIST
+    #undef CAN_LIST_DATA
 }
 
 CAN_ID_t get_CAN_ID_from_internal(uint16_t internal) {
@@ -23,14 +38,3 @@ CAN_ID_t get_CAN_ID_from_internal(uint16_t internal) {
 uint16_t get_internal_from_id_pos(uint16_t id, uint16_t pos) {
     return CAN_ID_POS_INTERNAL_ID_MAP[convert_id_pos_to_index(id, pos)];
 }
-
-// PRIMEIRA POSICAO DA MENSAGEM CAN N PODE ESTAR VAZIA
-
-
-#define CAN_LIST_DATA (var_name, msg_id, msg_wrd)\
-    CAN_ID_map[##var_name].id = ##msg_id;  \
-    CAN_ID_map[##var_name].pos = ##msg_wrd; \
-    CAN_LIST
-#undef CAN_LIST_DATA
-
-

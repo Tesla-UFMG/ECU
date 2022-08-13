7af60e9428fe100fc89f66be7b6a18cf3e32ff98
Luiza Araújo <luizaaraujo1020@gmail.com>
2022-08-13 16:54:44 -0300

    competition modifications

--- Core/Inc/sensors/APPS.h
+++ Core/Inc/sensors/APPS.h
@@ -22,24 +22,24 @@
 // Explicacoes e mais detalhes disponiveis na planilha:
 // https://docs.google.com/spreadsheets/d/1oN8rV0IG5MuoPPMHFQrklaCpWQLI31Kn/edit?usp=sharing&ouid=111087741893616112745&rtpof=true&sd=true
 
-#define APPS_MATRIX_LENGTH 10
+#define APPS_MATRIX_LENGTH 8
 // APPS 1
 #define APPS1_REF                                                                        \
-    { 2200, 2270, 2430, 2570, 2700, 2900, 3110, 3200, 3460, 3520 }
+    { 2240, 2320, 2430, 2680, 2860, 3030, 3120, 3440 }
 #define APPS1_FIX_MUL                                                                    \
-    { 0, 1.0526, 0.625, 0.7143, 0.7692, 0.5, 0.4762, 1.1111, 0.3846, 1.6667 }
+    { 0, 0.6896, 0.9091, 0.4, 0.5556, 0.5882, 1.1111, 0.3125 }
 #define APPS1_FIX_ADD                                                                    \
-    { 0, -2259.5, -1307.7, -1513.5, -1643.6, -905.6, -825.4, -2788.9, -453.1, -4877.9}
+    { 0, -1500, -1966.3, -686.4, -1060.5, -1111.1, -2652.7, -118.2}
 #define APPS1_MAX 3700
-#define APPS1_MIN 2000
+#define APPS1_MIN 0
 // APPS 2
 #define APPS2_REF                                                                        \
-    { 310, 600, 955, 1280, 1570, 2000, 2350, 2700, 3035, 3120 }
+    { 310, 815, 1140, 1460, 1800, 2070, 2540, 2930 }
 #define APPS2_FIX_MUL                                                                    \
-    { 0, 0.3125, 0.2817, 0.3077, 0.3448, 0.2326, 0.2857, 0.2857, 0.2985, 1.1765 }
+    { 0, 0.1905, 0.3077, 0.3125, 0.2941, 0.3704, 0.2128, 0.2564 }
 #define APPS2_FIX_ADD                                                                    \
-    { 0, -87.5, -57.9, -71.6, -108.1, 79.3, -15.9, -4.8, -28.3, -2681.8 }
+    { 0, -55.2, -107.9, -70.6, -1.01, -95.5, 273.6, 205.5 }
 #define APPS2_MAX 3300
-#define APPS2_MIN 100
+#define APPS2_MIN 0
 
 #endif /* INC_THROTTLE_H_ */
--- Core/Src/util/buttons_handler.c
+++ Core/Src/util/buttons_handler.c
@@ -19,19 +19,19 @@ static buttons_parameters_t buttons[BUTTONS_QUAN];
 
 void button_release_handler(available_buttons_e button) {
     switch (button) {
-        case B_RTD:
+        case B_MODE:
             osThreadFlagsSet(t_RTDHandle, RTD_BTN_PRESSED_FLAG);
             osThreadFlagsSet(t_pilot_resetHandle, RTD_BTN_PRESSED_FLAG);
             break;
 
-        case B_MODE:;
+        case B_DYNAMICS_CONTROLS:;
             RACE_MODE_t race_mode = get_global_var_value(RACE_MODE);
             set_global_var_value(RACE_MODE, race_mode + 1);
             osThreadFlagsSet(t_seleciona_modoHandle, MODE_BTN_PRESSED_FLAG);
             osThreadFlagsSet(t_pilot_resetHandle, MODE_BTN_PRESSED_FLAG);
             break;
 
-        case B_DYNAMICS_CONTROLS:; break;
+        case B_RTD:; break;
 
         default: break;
     }

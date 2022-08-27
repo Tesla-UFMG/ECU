7950b9316d4cd51c1ce12c02afffa4da909007d4
Luiza Araújo <luizaaraujo1020@gmail.com>
2022-08-27 18:43:28 -0300

    calibracao apps competicao

--- Core/Inc/sensors/APPS.h
+++ Core/Inc/sensors/APPS.h
@@ -22,23 +22,23 @@
 // Explicacoes e mais detalhes disponiveis na planilha:
 // https://docs.google.com/spreadsheets/d/1oN8rV0IG5MuoPPMHFQrklaCpWQLI31Kn/edit?usp=sharing&ouid=111087741893616112745&rtpof=true&sd=true
 
-#define APPS_MATRIX_LENGTH 8
+#define APPS_MATRIX_LENGTH 9
 // APPS 1
 #define APPS1_REF                                                                        \
-    { 2240, 2320, 2430, 2680, 2860, 3030, 3120, 3440 }
+    { 2200, 2300, 2445, 2605, 2751, 2910, 3040, 3210, 3345 }
 #define APPS1_FIX_MUL                                                                    \
-    { 0, 0.6896, 0.9091, 0.4, 0.5556, 0.5882, 1.1111, 0.3125 }
+    { 0, 1.0526, 0.6896, 0.625, 0.6849, 0.6289, 0.7692, 0.5882, 0.7407 }
 #define APPS1_FIX_ADD                                                                    \
-    { 0, -1500, -1966.3, -686.4, -1060.5, -1111.1, -2652.7, -118.2}
+    { 0, -2321, -1461.2, -1278.1, -1409.2, -1230.2, -1613.5, -1038.2, -1502.8 }
 #define APPS1_MAX 3700
 #define APPS1_MIN 0
 // APPS 2
 #define APPS2_REF                                                                        \
-    { 310, 815, 1140, 1460, 1800, 2070, 2540, 2930 }
+    { 300, 574, 872, 1205, 1530, 1860, 2155, 2465, 2828 }
 #define APPS2_FIX_MUL                                                                    \
-    { 0, 0.1905, 0.3077, 0.3125, 0.2941, 0.3704, 0.2128, 0.2564 }
+    { 0, 0.3650, 0.3356, 0.3003, 0.3077, 0.3030, 0.3390, 0.3226, 0.2755 }
 #define APPS2_FIX_ADD                                                                    \
-    { 0, -55.2, -107.9, -70.6, -1.01, -95.5, 273.6, 205.5 }
+    { 0, -109.5, -67.6, -11.9, 4.2, 36.4, -5.5, 54.8, 195.9 }
 #define APPS2_MAX 3300
 #define APPS2_MIN 0
 

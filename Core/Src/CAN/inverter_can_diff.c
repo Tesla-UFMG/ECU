1f718dc4cee3999e1a42828dbb45f1829ef60258
caiussouza <caiuscsouza@gmail.com>
2023-01-30 14:07:41 -0300

    correcoes PR

--- Core/Src/CAN/inverter_can_diff.c
+++ Core/Src/CAN/inverter_can_diff.c
@@ -11,6 +11,9 @@
 #include "util/global_variables.h"
 #include "util/util.h"
 
+#define POS_LEFT_INV_ERROR 1
+#define POS_RIGHT_INV_ERROR 2
+
 
 static FDCAN_RxHeaderTypeDef RxHeader;
 
@@ -22,23 +25,46 @@ void inverter_diff(void* argument) {
     for (;;) {
         ECU_ENABLE_BREAKPOINT_DEBUG();
 
-        uint32_t id = RxHeader.Identifier;
+        uint32_t id;	//pq foi inicializada com esse valor?
+        const bool left_inv_received;
+        const bool right_inv_received;
+        const bool left_inv_error;
+        const bool right_inv_error;
+        uint8_t inv_error_type;
 
-        osMessageQueueGet(q_ids_can_inverterHandle, &id, NULL, osWaitForever);
+        //while(timer aleatorio){  <- COLOCO ESSE WHILE AQ?
 
-        bool right_inv_received = false;
-        bool left_inv_received  = false;
+        	id = RxHeader.Identifier;
 
-        if (id == ID_RIGHT_INVERTER) {
-            right_inv_received = true;
+        	osMessageQueueGet(q_ids_can_inverterHandle, &id, NULL, osWaitForever);
+
+        	left_inv_received = (id  >= 100 && id <= 103);
+        	right_inv_received = (id  >= 200 && id <= 203);
+
         }
 
-        if (id == ID_LEFT_INVERTER) {
-            left_inv_received = true;
+        left_inv_error = (!left_inv_received);
+        right_inv_error = (!right_inv_received);
+
+        if	(left_inv_error) {
+        	inv_error_type = POS_LEFT_INV_ERROR;
         }
 
-        if ((left_inv_received && right_inv_received) == true) {
-            osThreadFlagsSet(t_inverter_comm_errorHandle, INVERTER_CAN_ACTIVE);
+        if	(right_inv_error) {
+        	inv_error_type = POS_RIGHT_INV_ERROR;
+        }
+
+        //while(timer aleatorio){  <- OU COLOCO ESSE WHILE AQ?
+        switch(inv_error_type){
+        	case POS_LEFT_INV_ERROR:
+				//manda esse erro pra can monitor sla?
+				break;
+        	case POS_RIGHT_INV_ERROR:
+				//rt
+				break;
+        	default:
+        		osThreadFlagsSet(t_inverter_comm_errorHandle, INVERTER_CAN_ACTIVE);
+
         }
     }
 }

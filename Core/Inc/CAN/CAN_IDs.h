ff6bc57e4bd57f35963697a3c71d1a614c3a5083
caiussouza <caiuscsouza@gmail.com>
2023-04-20 12:05:00 -0300

    atualizando ids de can

--- Core/Inc/CAN/CAN_IDs.h
+++ Core/Inc/CAN/CAN_IDs.h
@@ -20,63 +20,73 @@
 // DATALOGGER
 // LEGENDA:  LABEL    	    	 INTERNAL_ID
 // 0 E RESERVADO PARA LACUNAS NO PACOTE
-#define ID_ECU_TIMER            1
+#define ID_SPEED_AVG            1
 #define ID_STEERING_WHEEL       2
 #define ID_THROTTLE             3
 #define ID_BRAKE                4
 #define ID_MODE                 5
-#define ID_DISTANCE_P_ODOM      6
-#define ID_DISTANCE_T_ODOM      7
-#define ID_TORQUE_R_MOTOR       8
-#define ID_TORQUE_L_MOTOR       9
-#define ID_REF_TORQUE_R_MOTOR   10
-#define ID_REF_TORQUE_L_MOTOR   11
-#define ID_REF_SPEED_R          12
-#define ID_REF_SPEED_L          13
-#define ID_CONTROL_EVENT_FLAG_1 14
-#define ID_CONTROL_EVENT_FLAG_2 15
-#define ID_SPEED_FL             16
-#define ID_SPEED_FR             17
-#define ID_SPEED_RR             18
-#define ID_SPEED_RL             19
-#define ID_TORQUE_GAIN          20
-#define ID_CURRENT_R_MOTOR      21
-#define ID_CURRENT_L_MOTOR      22
-#define ID_TEMPERATURE1_R       23
-#define ID_TEMPERATURE2_R       24
-#define ID_TEMPERATURE1_L       25
-#define ID_TEMPERATURE2_L       26
-#define ID_IMU_BSE_ERROR        27
-#define ID_STATUS_DATALOG       28
-#define ID_IMU_SPEED_ERROR      29
-#define ID_CURRENT_FLAG         30
-#define ID_PANEL_DEBUG_1        31
-#define ID_PANEL_DEBUG_2        32
-#define ID_PANEL_DEBUG_3        33
-#define ID_PANEL_DEBUG_4        34
-#define ID_REGEN_BRAKE_STATE    35
-#define ID_SPEED_L_MOTOR        36
-#define ID_SPEED_R_MOTOR        37
-#define ID_POWER_L_MOTOR        38
-#define ID_POWER_R_MOTOR        39
-#define ID_ENERGY_L_MOTOR       40
-#define ID_ENERGY_R_MOTOR       41
-#define ID_OVERLOAD_L_MOTOR     42
-#define ID_OVERLOAD_R_MOTOR     43
-#define ID_LOST_MSG_L_MOTOR     44
-#define ID_LOST_MSG_R_MOTOR     45
-#define ID_BUS_OFF_L_MOTOR      46
-#define ID_BUS_OFF_R_MOTOR      47
-#define ID_CAN_STATE_L_MOTOR    48
-#define ID_CAN_STATE_R_MOTOR    49
-#define ID_INV_STATE_L_MOTOR    50
-#define ID_INV_STATE_R_MOTOR    51
-#define ID_FAILURE_L_MOTOR      52
-#define ID_FAILURE_R_MOTOR      53
-#define ID_ALARM_L_MOTOR        54
-#define ID_ALARM_R_MOTOR        55
+#define ID_TORQUE_GAIN          6
+#define ID_DISTANCE_P_ODOM      7
+#define ID_DISTANCE_T_ODOM      8
+#define ID_CONTROL_EVENT_FLAG_1 9
+#define ID_CONTROL_EVENT_FLAG_2 10
+#define ID_REF_TORQUE_R_MOTOR   11
+#define ID_REF_TORQUE_L_MOTOR   12
+#define ID_SPEED_FL             13
+#define ID_SPEED_FR             14
+#define ID_SPEED_RL             15
+#define ID_SPEED_RR             16
+// #define /*RED_TORQUE_ROD_ESQ */ 17
+// #define /*RED_TORQUE_ROD_DIR */ 18
+// #define /*EXTRA_CONTROL_DIN  */ 19
+// #define /*EXTRA_CONTROL_DIN  */ 20
+#define ID_REGEN_BRAKE_STATE    21
+#define ID_IMU_BSE_ERROR        22
+#define ID_STATUS_DATALOG       23
+#define ID_IMU_SPEED_ERROR      24
+#define ID_CURRENT_FLAG         25
+#define ID_PANEL_DEBUG_1        26
+#define ID_PANEL_DEBUG_2        27
+#define ID_PANEL_DEBUG_3        28
+#define ID_PANEL_DEBUG_4        29
+#define ID_SPEED_L_MOTOR        30
+#define ID_TORQUE_L_MOTOR       31
+#define ID_POWER_L_MOTOR        32
+#define ID_CURRENT_L_MOTOR      33
+#define ID_ENERGY_L_MOTOR       34
+#define ID_OVERLOAD_L_MOTOR     35
+#define ID_TEMPERATURE1_L       36
+#define ID_TEMPERATURE2_L       37
+#define ID_LOST_MSG_L_MOTOR     38
+#define ID_BUS_OFF_L_MOTOR      39
+#define ID_CAN_STATE_L_MOTOR    40
+#define ID_INV_STATE_L_MOTOR    41
+#define ID_FAILURE_L_MOTOR      42
+#define ID_ALARM_L_MOTOR        43
+#define ID_SPEED_R_MOTOR        44
+#define ID_TORQUE_R_MOTOR       45
+#define ID_POWER_R_MOTOR        46
+#define ID_CURRENT_R_MOTOR      47
+#define ID_ENERGY_R_MOTOR       48
+#define ID_OVERLOAD_R_MOTOR     49
+#define ID_TEMPERATURE1_R       50
+#define ID_TEMPERATURE2_R       51
+#define ID_LOST_MSG_R_MOTOR     52
+#define ID_BUS_OFF_R_MOTOR      53
+#define ID_CAN_STATE_R_MOTOR    54
+#define ID_INV_STATE_R_MOTOR    55
+#define ID_FAILURE_R_MOTOR      56
+#define ID_ALARM_R_MOTOR        57
+// #define /*ACCEL X*/ 58
+// #define /*ACCEL Y*/ 59
+// #define /*ACCEL Z*/ 60
+// #define /*TEMP IMU*/ 61
+// #define /*GYRO X*/ 62
+// #define /*GYRO Y*/ 63
+// #define /*GYRO Z*/ 64
+// #define /*IMU ERROR*/ 65
 
-#define CAN_ID_QUAN 56
+#define CAN_ID_QUAN 65
 
 typedef struct {
     uint16_t id;
@@ -87,11 +97,11 @@ extern CAN_ID_t CAN_ID_map[CAN_ID_QUAN];
 // TODO(renanmoreira): IMPLEMENT X-MACROS FOR CAN_ID'S
 #define ECU_CAN_INITIAL_ID        100
 #define ECU_CAN_FINAL_ID          149
-#define ECU_CAN_LAST_POPULATED_ID 113
+#define ECU_CAN_LAST_POPULATED_ID 131
 #define ECU_CAN_FIRST_DEBUG_ID    351
 #define ECU_CAN_LAST_DEBUG_ID     351
 
-#define INTERNAL_ID_ECU_TIMER                                                            \
+#define INTERNAL_ID_SPEED_AVG                                                            \
     { 101, 0 }
 #define INTERNAL_ID_STEERING_WHEEL                                                       \
     { 101, 1 }
@@ -101,48 +111,38 @@ extern CAN_ID_t CAN_ID_map[CAN_ID_QUAN];
     { 101, 3 }
 #define INTERNAL_ID_MODE                                                                 \
     { 102, 0 }
+#define INTERNAL_ID_TORQUE_GAIN                                                          \
+    { 102, 1 }
 #define INTERNAL_ID_DISTANCE_P_ODOM                                                      \
     { 102, 2 }
 #define INTERNAL_ID_DISTANCE_T_ODOM                                                      \
     { 102, 3 }
-#define INTERNAL_ID_TORQUE_R_MOTOR                                                       \
+#define INTERNAL_ID_CONTROL_EVENT_FLAG_1                                                 \
     { 103, 0 }
-#define INTERNAL_ID_TORQUE_L_MOTOR                                                       \
+#define INTERNAL_ID_CONTROL_EVENT_FLAG_2                                                 \
     { 103, 1 }
 #define INTERNAL_ID_REF_TORQUE_R_MOTOR                                                   \
     { 103, 2 }
 #define INTERNAL_ID_REF_TORQUE_L_MOTOR                                                   \
     { 103, 3 }
-#define INTERNAL_ID_REF_SPEED_R_MOTOR                                                    \
-    { 104, 0 }
-#define INTERNAL_ID_REF_SPEED_L_MOTOR                                                    \
-    { 104, 1 }
-#define INTERNAL_ID_CONTROL_EVENT_FLAG_1                                                 \
-    { 104, 2 }
-#define INTERNAL_ID_CONTROL_EVENT_FLAG_2                                                 \
-    { 104, 3 }
 #define INTERNAL_ID_SPEED_FL                                                             \
-    { 105, 0 }
+    { 104, 0 }
 #define INTERNAL_ID_SPEED_FR                                                             \
-    { 105, 1 }
-#define INTERNAL_ID_SPEED_RR                                                             \
-    { 105, 2 }
+    { 104, 1 }
 #define INTERNAL_ID_SPEED_RL                                                             \
-    { 105, 3 }
-#define INTERNAL_ID_TORQUE_GAIN                                                          \
+    { 104, 2 }
+#define INTERNAL_ID_SPEED_RR                                                             \
+    { 104, 3 }
+// #define REDUCAO TORQUE RODA ESQUERDA                                                   \
+//     { 105, 0 }
+// #define  || || || DIREITA                                                              \
+//     { 105, 1 }
+// #define  EXTRA CONTROLE DINAMICO   flags crossval?                                     \
+//     { 105, 2 }
+// #define  EXTRA CONTROLE DINAMICO                                                       \
+//     { 105, 3 }
+#define INTERNAL_ID_REGEN_BRAKE_STATE                                                    \
     { 106, 0 }
-#define INTERNAL_ID_CURRENT_R_MOTOR                                                      \
-    { 106, 2 }
-#define INTERNAL_ID_CURRENT_L_MOTOR                                                      \
-    { 106, 3 }
-#define INTERNAL_ID_TEMPERATURE1_R                                                       \
-    { 107, 0 }
-#define INTERNAL_ID_TEMPERATURE2_R                                                       \
-    { 107, 1 }
-#define INTERNAL_ID_TEMPERATURE1_L                                                       \
-    { 107, 2 }
-#define INTERNAL_ID_TEMPERATURE2_L                                                       \
-    { 107, 3 }
 #define INTERNAL_ID_IMU_BSE_ERROR                                                        \
     { 108, 0 }
 #define INTERNAL_ID_STATUS_DATALOG                                                       \
@@ -159,48 +159,78 @@ extern CAN_ID_t CAN_ID_map[CAN_ID_QUAN];
     { 351, 2 }
 #define INTERNAL_ID_PANEL_DEBUG_4                                                        \
     { 351, 3 }
-#define INTERNAL_ID_REGEN_BRAKE_STATE                                                    \
-    { 102, 1 }
 #define INTERNAL_ID_SPEED_L_MOTOR                                                        \
-    { 109, 0 }
-#define INTERNAL_ID_SPEED_R_MOTOR                                                        \
-    { 109, 1 }
-#define INTERNAL_ID_POWER_L_MOTOR                                                        \
-    { 109, 2 }
-#define INTERNAL_ID_POWER_R_MOTOR                                                        \
-    { 109, 3 }
-#define INTERNAL_ID_ENERGY_L_MOTOR                                                       \
     { 110, 0 }
-#define INTERNAL_ID_ENERGY_R_MOTOR                                                       \
+#define INTERNAL_ID_TORQUE_L_MOTOR                                                       \
     { 110, 1 }
-#define INTERNAL_ID_OVERLOAD_L_MOTOR                                                     \
+#define INTERNAL_ID_POWER_L_MOTOR                                                        \
     { 110, 2 }
-#define INTERNAL_ID_OVERLOAD_R_MOTOR                                                     \
+#define INTERNAL_ID_CURRENT_L_MOTOR                                                      \
     { 110, 3 }
-#define INTERNAL_ID_LOST_MSG_L_MOTOR                                                     \
+#define INTERNAL_ID_ENERGY_L_MOTOR                                                       \
     { 111, 0 }
-#define INTERNAL_ID_LOST_MSG_R_MOTOR                                                     \
+#define INTERNAL_ID_OVERLOAD_L_MOTOR                                                     \
     { 111, 1 }
-#define INTERNAL_ID_BUS_OFF_L_MOTOR                                                      \
+#define INTERNAL_ID_TEMPERATURE1_L                                                       \
     { 111, 2 }
-#define INTERNAL_ID_BUS_OFF_R_MOTOR                                                      \
+#define INTERNAL_ID_TEMPERATURE2_L                                                       \
     { 111, 3 }
-#define INTERNAL_ID_CAN_STATE_L_MOTOR                                                    \
+#define INTERNAL_ID_LOST_MSG_L_MOTOR                                                     \
     { 112, 0 }
-#define INTERNAL_ID_CAN_STATE_R_MOTOR                                                    \
+#define INTERNAL_ID_BUS_OFF_L_MOTOR                                                      \
     { 112, 1 }
-#define INTERNAL_ID_INV_STATE_L_MOTOR                                                    \
+#define INTERNAL_ID_CAN_STATE_L_MOTOR                                                    \
     { 112, 2 }
-#define INTERNAL_ID_INV_STATE_R_MOTOR                                                    \
-    { 112, 3 }
-#define INTERNAL_ID_FAILURE_L_MOTOR                                                      \
+#define INTERNAL_ID_INV_STATE_L_MOTOR                                                    \
     { 113, 0 }
-#define INTERNAL_ID_FAILURE_R_MOTOR                                                      \
+#define INTERNAL_ID_FAILURE_L_MOTOR                                                      \
     { 113, 1 }
 #define INTERNAL_ID_ALARM_L_MOTOR                                                        \
     { 113, 2 }
+#define INTERNAL_ID_SPEED_R_MOTOR                                                        \
+    { 120, 0 }
+#define INTERNAL_ID_TORQUE_R_MOTOR                                                       \
+    { 120, 1 }
+#define INTERNAL_ID_POWER_R_MOTOR                                                        \
+    { 120, 2 }
+#define INTERNAL_ID_CURRENT_R_MOTOR                                                      \
+    { 120, 3 }
+#define INTERNAL_ID_ENERGY_R_MOTOR                                                       \
+    { 121, 0 }
+#define INTERNAL_ID_OVERLOAD_R_MOTOR                                                     \
+    { 121, 1 }
+#define INTERNAL_ID_TEMPERATURE1_R                                                       \
+    { 121, 2 }
+#define INTERNAL_ID_TEMPERATURE2_R                                                       \
+    { 121, 3 }
+#define INTERNAL_ID_LOST_MSG_R_MOTOR                                                     \
+    { 122, 0 }
+#define INTERNAL_ID_BUS_OFF_R_MOTOR                                                      \
+    { 122, 1 }
+#define INTERNAL_ID_CAN_STATE_R_MOTOR                                                    \
+    { 122, 2 }
+#define INTERNAL_ID_INV_STATE_R_MOTOR                                                    \
+    { 123, 0 }
+#define INTERNAL_ID_FAILURE_R_MOTOR                                                      \
+    { 123, 1 }
 #define INTERNAL_ID_ALARM_R_MOTOR                                                        \
-    { 113, 3 }
+    { 123, 2 }
+// #define ACCEL X                                                                        \
+//     { 130, 0 }
+// #define ACCEL Y                                                                        \
+//     { 130, 1 }
+// #define ACCEL Z                                                                        \
+//     { 130, 2 }
+// #define TEMP IMU                                                                       \
+//     { 130, 3 }
+// #define GYRO X                                                                         \
+//     { 131, 0 }
+// #define GYRO Y                                                                         \
+//     { 131, 1 }
+// #define GYRO Z                                                                         \
+//     { 131, 2 }
+// #define IMU ERROR                                                                      \
+//     { 131, 3 }
 
 //----------
 

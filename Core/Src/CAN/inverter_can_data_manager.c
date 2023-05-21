706d728ca6d27c666535ed2ae111f08b9c12e059
Felipe Telles <felipe.melo.telles@gmail.com>
2023-05-21 14:52:53 -0300

    inverter macro fix

--- Core/Src/CAN/inverter_can_data_manager.c
+++ Core/Src/CAN/inverter_can_data_manager.c
@@ -21,16 +21,11 @@ uint16_t inverter_get_value(can_vars_inverter_e var_name) {
 }
 
 can_vars_inverter_e inverter_get_var_name_from_id_and_pos(uint32_t id, int pos) {
-    // Unused because compiler may think this is unused and throw a warning as it does not
-    // implement X-MACRO
-    UNUSED(id);
-    UNUSED(pos);
 #define ENTRY(a, b, c)                                                                   \
-    if (id == (b) && pos == (c))                                                         \
+    if (id == (b) && pos == (c)) {                                                       \
         return a;                                                                        \
-    else {                                                                               \
-        VARIABLES_INVERTER_CAN_RX;                                                       \
-    }
+    } else
+    VARIABLES_INVERTER_CAN_RX;
 #undef ENTRY
     return INVALID_VARIABLE_INVERTER;
 }

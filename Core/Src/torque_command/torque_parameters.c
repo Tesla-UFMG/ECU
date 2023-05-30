a54051c33d032d620c00e00cd278dd08349d555c
dmrohrs <dmrohrs@gmail.com>
2023-05-30 18:14:27 -0300

    regen_brake_ramp_function_tested!

--- Core/Src/torque_command/torque_parameters.c
+++ Core/Src/torque_command/torque_parameters.c
@@ -14,35 +14,50 @@
 #include "util/global_definitions.h"
 #include "util/global_instances.h"
 #include "util/util.h"
+#include "CAN/inverter_can_data_manager.h"
+#include "stdio.h"
 
 void update_regen_state(vehicle_state_e vehicle_state);
 
 extern osMessageQueueId_t q_ref_torque_messageHandle;
-//extern UART_HandleTypeDef hlpuart1;
+extern UART_HandleTypeDef hlpuart1;
 
 volatile vehicle_state_parameters_t g_vehicle_state_parameters;
 
 volatile vehicle_state_e vehicle_state;
 
 // Regen brake tests variables
+uint16_t MAX_REG_BRAKING_TORQUE = 2000;
+uint16_t regen_power = 6000*10;
+#define equation_constant 45.22
+uint16_t REG_BRAKING_TORQUE = 0;
+uint16_t max_speed = 0;
+bool regenerating = false;
+
+void constant_power_torque_calc()
+{
+	max_speed = max(inverter_get_value(speed_m_l), inverter_get_value(speed_m_r));
+	REG_BRAKING_TORQUE = ((regen_power*equation_constant)/max_speed);
+	REG_BRAKING_TORQUE = min(REG_BRAKING_TORQUE, MAX_REG_BRAKING_TORQUE);
+}
+
 //uint16_t regenerative_cc_current = 0;
 //bool speed_condition = false;
-//bool regenerating = false;
 
-//int digit_counter (uint16_t number)
-//{
-//    int counter = 0;
-//    if (number == 0)
-//        return 1;
-//    while (number != 0)
-//    {
-//        number /= 10;
-//        counter++;
-//    }
-//    // printf("contdor parcial %d\n", contador);
-//    return counter;
-//}
-//
+int digit_counter (uint16_t number)
+{
+    int counter = 0;
+    if (number == 0)
+        return 1;
+    while (number != 0)
+    {
+        number /= 10;
+        counter++;
+    }
+    // printf("contdor parcial %d\n", contador);
+    return counter;
+}
+
 //uint16_t two_complement_transform(uint16_t number)
 //{
 //	uint16_t bits_size = 16;
@@ -63,25 +78,25 @@ void update_state(bool disable) {
     if (disable == true) {
         vehicle_state = S_DISABLE_E;
         //speed_condition = false;
-        //REG_BRAKING_TORQUE = 0;
-        //max_speed = 0;
+        REG_BRAKING_TORQUE = 0;
+        max_speed = 0;
     } else if ((get_global_var_value(THROTTLE_PERCENT) < 100)
                && (frenagem_regenerativa == true)
-               && get_global_var_value(REAR_AVG_SPEED) > RPM_KMPH_5) {
+               && get_global_var_value(REAR_AVG_SPEED) > 1200) {
         vehicle_state = S_BRAKE_E;
-        //constant_power_torque_calc();
+        constant_power_torque_calc();
         //speed_condition = true; //teste pra ver se ta entrando
 
     } else if (get_global_var_value(THROTTLE_PERCENT) > 100) {
         vehicle_state = S_ACCELERATE_E;
         //speed_condition = false;
-        //REG_BRAKING_TORQUE = 0;
-        //max_speed = 0;
+        REG_BRAKING_TORQUE = 0;
+        max_speed = 0;
     } else {
         vehicle_state = S_NEUTER_E;
         //speed_condition = false;
-        //REG_BRAKING_TORQUE = 0;
-        //max_speed = 0;
+        REG_BRAKING_TORQUE = 0;
+        max_speed = 0;
     }
 
     update_regen_state(vehicle_state);
@@ -111,8 +126,8 @@ void update_state_parameters(torque_message_t* torque_message) {
             set_bit8(&torque_message->parameters, P_RUNSTOP, true);
             torque_message->torque_ref[R_MOTOR]     = 0;
             torque_message->torque_ref[L_MOTOR]     = 0;
-            torque_message->neg_torque_ref[R_MOTOR] = REG_BRAKING_TORQUE;
-            torque_message->neg_torque_ref[L_MOTOR] = REG_BRAKING_TORQUE;
+            torque_message->neg_torque_ref[R_MOTOR] = REG_BRAKING_TORQUE/*100*/;
+            torque_message->neg_torque_ref[L_MOTOR] = REG_BRAKING_TORQUE/*100*/;
             torque_message->speed_ref[R_MOTOR]      = 0;
             torque_message->speed_ref[L_MOTOR]      = 0;
             break;
@@ -148,7 +163,7 @@ void torque_parameters(void* argument) {
     ref_torque_t ref_torque_message;
     // complete message to be sent to inverter
     torque_message_t torque_message = {.parameters = 0};
-    //char buffer [25] = {0};
+    char buffer [35] = {0};
 
     for (;;) {
 
@@ -179,26 +194,28 @@ void torque_parameters(void* argument) {
                 log_data(ID_REF_TORQUE_L_MOTOR, torque_message.torque_ref[L_MOTOR]);
                 //regenerative_cc_current = calculate_inverter_cc_current();
 
-//                uint16_t int_left_motor_rpm = inverter_get_value(speed_m_l) /*1000*/;
-//                uint16_t int_right_motor_rpm = inverter_get_value(speed_m_r) /*2000*/;
-//                uint16_t int_left_motor_torque = inverter_get_value(torque_m_l) /*300*/;
-//                uint16_t int_right_motor_torque = inverter_get_value(torque_m_r) /*4000*/;
-//                int cont1 = digit_counter(int_left_motor_rpm);
-//                int cont2 = digit_counter(int_right_motor_rpm);
-//                int cont3 = digit_counter(int_left_motor_torque);
-//                int cont4 = digit_counter(int_right_motor_torque);
-//                int total_cont = cont1 + cont2 + cont3 + cont4;
-//                snprintf(buffer, (total_cont +5), "%u,%u,%u,%u-", int_left_motor_rpm, int_right_motor_rpm, int_left_motor_torque, int_right_motor_torque);
-//                HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, (total_cont+5), 500);
+                uint16_t int_left_motor_rpm = inverter_get_value(speed_m_l) /*1000*/;
+                uint16_t int_right_motor_rpm = inverter_get_value(speed_m_r) /*2000*/;
+                uint16_t int_left_motor_torque = inverter_get_value(torque_m_l) /*300*/;
+                uint16_t int_right_motor_torque = inverter_get_value(torque_m_r) /*4000*/;
+                uint16_t int_neg_ref = REG_BRAKING_TORQUE /*2000*/;
+                int cont1 = digit_counter(int_left_motor_rpm);
+                int cont2 = digit_counter(int_right_motor_rpm);
+                int cont3 = digit_counter(int_left_motor_torque);
+                int cont4 = digit_counter(int_right_motor_torque);
+                int cont5 = digit_counter(int_neg_ref);
+                int total_cont = cont1 + cont2 + cont3 + cont4+ cont5;
+                snprintf(buffer, (total_cont +6), "%u,%u,%u,%u,%u-", int_left_motor_rpm, int_right_motor_rpm, int_left_motor_torque, int_right_motor_torque, int_neg_ref);
+                HAL_UART_Transmit(&hlpuart1, (uint8_t *)buffer, (total_cont+5), 50);
 
-//                if ((inverter_get_value(current_m_l) & (1<<(15))&& (inverter_get_value(current_m_r) & (1<<15))))
-//                {
-//                	regenerating = true;
-//                }
-//                else
-//                {
-//                	regenerating = false;
-//                }
+                if ((inverter_get_value(current_m_l) & (1<<(15))&& (inverter_get_value(current_m_r) & (1<<15))))
+                {
+                	regenerating = true;
+                }
+                else
+                {
+                	regenerating = false;
+                }
 
                 break;
             case osErrorTimeout:

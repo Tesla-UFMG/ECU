0d687fcbfcfdb5bf2be63121c095527d64027b5a
dmrohrs <dmrohrs@gmail.com>
2023-07-27 14:09:39 -0300

    format changes

--- Core/Src/datalogging/odometer_calc.c
+++ Core/Src/datalogging/odometer_calc.c
@@ -18,57 +18,62 @@
 #include "util/global_instances.h"
 #include "util/util.h"
 
-//static inline uint32_t calculate_distance(uint32_t speed_avg);
-//static void log_distance(uint32_t total_dist, uint32_t partial_dist);
+// static inline uint32_t calculate_distance(uint32_t speed_avg);
+// static void log_distance(uint32_t total_dist, uint32_t partial_dist);
 
 void odometer_calc() {
 
-//    uint32_t partial_dist_traveled = 0;
-//    odometer_message_t total_dist_traveled;
-//    uint16_t odometer_speed_data = 0;
-//
-//    // Read distance from flash just once
-//    Flash_Read_Data(ODOMETER_DATA_FLASH_ADDR, &(total_dist_traveled), WORDS_READ_ONE);
-//
-//    // In case the memory position has never been used/saved
-//    if ((total_dist_traveled == 0x00000000) || (total_dist_traveled == 0xFFFFFFFF)) {
-//        total_dist_traveled = 0;
-//    }
-//    total_dist_traveled = m_to_cm(total_dist_traveled);
-//    osMessageQueuePutOverwrite(q_odometer_calc_save_messageHandle, &total_dist_traveled,
-//                               0);
+    //    uint32_t partial_dist_traveled = 0;
+    //    odometer_message_t total_dist_traveled;
+    //    uint16_t odometer_speed_data = 0;
+    //
+    //    // Read distance from flash just once
+    //    Flash_Read_Data(ODOMETER_DATA_FLASH_ADDR, &(total_dist_traveled),
+    //    WORDS_READ_ONE);
+    //
+    //    // In case the memory position has never been used/saved
+    //    if ((total_dist_traveled == 0x00000000) || (total_dist_traveled == 0xFFFFFFFF))
+    //    {
+    //        total_dist_traveled = 0;
+    //    }
+    //    total_dist_traveled = m_to_cm(total_dist_traveled);
+    //    osMessageQueuePutOverwrite(q_odometer_calc_save_messageHandle,
+    //    &total_dist_traveled,
+    //                               0);
 
     for (;;) {
         ECU_ENABLE_BREAKPOINT_DEBUG();
 
         wait_for_rtd();
-//        // Calculate and log distance traveled
-//        const SPEEDS_t speed_var = get_global_var_value(SPEEDS);
-//
-//        if (speed_var.wheels[FRONT_RIGHT] != 0 && speed_var.wheels[FRONT_LEFT] != 0) {
-//            get_global_var(FRONT_AVG_SPEED, &odometer_speed_data);
-//        } else {
-//            if (speed_var.wheels[FRONT_LEFT] == 0) {
-//                odometer_speed_data = speed_var.wheels[FRONT_RIGHT];
-//            } else {
-//                odometer_speed_data = speed_var.wheels[FRONT_LEFT];
-//            }
-//        }
-//
-//        const uint16_t instant_distant_traveled = calculate_distance(odometer_speed_data);
-//        partial_dist_traveled += instant_distant_traveled;
-//        total_dist_traveled += instant_distant_traveled;
-//        osMessageQueuePutOverwrite(q_odometer_calc_save_messageHandle,
-//                                   &total_dist_traveled, 0);
-//        log_distance(total_dist_traveled, partial_dist_traveled);
+        //        // Calculate and log distance traveled
+        //        const SPEEDS_t speed_var = get_global_var_value(SPEEDS);
+        //
+        //        if (speed_var.wheels[FRONT_RIGHT] != 0 && speed_var.wheels[FRONT_LEFT]
+        //        != 0) {
+        //            get_global_var(FRONT_AVG_SPEED, &odometer_speed_data);
+        //        } else {
+        //            if (speed_var.wheels[FRONT_LEFT] == 0) {
+        //                odometer_speed_data = speed_var.wheels[FRONT_RIGHT];
+        //            } else {
+        //                odometer_speed_data = speed_var.wheels[FRONT_LEFT];
+        //            }
+        //        }
+        //
+        //        const uint16_t instant_distant_traveled =
+        //        calculate_distance(odometer_speed_data); partial_dist_traveled +=
+        //        instant_distant_traveled; total_dist_traveled +=
+        //        instant_distant_traveled;
+        //        osMessageQueuePutOverwrite(q_odometer_calc_save_messageHandle,
+        //                                   &total_dist_traveled, 0);
+        //        log_distance(total_dist_traveled, partial_dist_traveled);
         osDelay(CALC_DELAY);
     }
 }
 
-//static void log_distance(uint32_t total_dist, uint32_t partial_dist) {
-//    log_data(ID_DISTANCE_T_ODOM, (uint16_t)cm_to_m(total_dist));
-//    log_data(ID_DISTANCE_P_ODOM, (uint16_t)cm_to_m(partial_dist));
-//}
+// static void log_distance(uint32_t total_dist, uint32_t partial_dist) {
+//     log_data(ID_DISTANCE_T_ODOM, (uint16_t)cm_to_m(total_dist));
+//     log_data(ID_DISTANCE_P_ODOM, (uint16_t)cm_to_m(partial_dist));
+// }
 
 static inline uint32_t calculate_distance(uint32_t speed_avg) {
     /*

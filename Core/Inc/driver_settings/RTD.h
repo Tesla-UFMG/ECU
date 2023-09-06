7285fa35326ad9b489f049a1ce29d10bbb30e4df
dmrohrs <dmrohrs@gmail.com>
2023-09-05 21:18:46 -0300

    changes made during Electrical Inspection

--- Core/Inc/driver_settings/RTD.h
+++ Core/Inc/driver_settings/RTD.h
@@ -9,6 +9,7 @@
 #define INC_RTD_H_
 
 // FSAE Rules: EV.10.5.2.a (2023)
+// Note : Judge specifically requested to use maximum time during Electrical Inspection
 #define RTDS_TIME_MS 3000
 
 void exit_RTD();

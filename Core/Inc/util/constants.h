24da7ce973cddbe0346fde45826242a9537fc34c
JoaoGabrielAbreuMegali <jgmegali@gmail.com>
2026-02-20 11:41:35 -0300

    Adicionei e traduzi comentários. Os valores das variáveis ainda precisma ser atualizadas.

--- Core/Inc/util/constants.h
+++ Core/Inc/util/constants.h
@@ -8,8 +8,8 @@
 #ifndef INC_CONSTANTS_H_
 #define INC_CONSTANTS_H_
 
-#define num_amostras_filtro   10   // Amostra media movel acelerador
-#define TORQUE_MAX            3000 // em decimos de percentagem do torque nom
+#define num_amostras_filtro   10		   // Moving average sample of the accelerator.
+#define TORQUE_MAX            3000 		   // In tenth of a percent of the nominal torque.
 #define vel_max_rpm           9000
 #define vetorizacao_torque    0
 #define frenagem_regenerativa 0
@@ -18,12 +18,12 @@
 #define modo_enduro           0
 #define zona_morta_acelerador 10
 #define TORQUE_GAIN           1
-#define ZERO_VOLANTE          662.9894
-#define GANHO_VOLANTE         0.9722419
-#define VOLANTE_ALINHADO      1030
-#define SPAN_ALINHAMENTO      70
-#define VOLANTE_MAX           3189
-#define VOLANTE_MIN           700
+#define ZERO_VOLANTE          662.9894	   // Bit value corresponding to 0 V at the sensor output; used to calibrate the measurement scaling.
+#define GANHO_VOLANTE         0.9722419	   // Controls the sensor gain; used to calibrate the sensitivity and measurement scaling.
+#define VOLANTE_ALINHADO      1030		   // ADC value when the steering wheel is in neutral position.
+#define SPAN_ALINHAMENTO      70           // Sensor Dead zone.
+#define VOLANTE_MAX           3189		   // Maximum value that the sensor can achieve once installed.
+#define VOLANTE_MIN           700          // Minimum value that the sensor can achieve once installed.
 #define GAIN_ARRANC           1.1
 #define INC_VELOC             50
 #define DIREITA               0

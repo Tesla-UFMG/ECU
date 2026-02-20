bdfea031ab0347f1e25fc095f04210233d647aa1
JoaoGabrielAbreuMegali <jgmegali@gmail.com>
2026-02-20 12:08:11 -0300

    RTD.C - Correção na função set_RTD

--- Core/Src/driver_settings/RTD.c
+++ Core/Src/driver_settings/RTD.c
@@ -99,8 +99,8 @@ static bool can_RTD_be_enabled() {
     return false;
 }
 
-static void set_RTD() {10
-    osEventFlagsSet(e_ECU_control_flagsHandle, RTD_FLAG);
+static void set_RTD() {
+	osEventFlagsSet(e_ECU_control_flagsHandle, RTD_FLAG);
     set_rgb_led(get_global_var_value(SELECTED_MODE).cor, FIXED);
     activate_RTDS();
 }

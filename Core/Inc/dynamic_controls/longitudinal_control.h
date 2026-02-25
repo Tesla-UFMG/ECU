652363bb110d623d2ac298eecca1a7f597993f7e
Guilherme Oliveira Porto de Paula <guilhermeoppaula@gmail.com>
2026-02-24 21:13:20 -0300

    longitudinal_control.h: Adicionando alguns comentários ao código

--- Core/Inc/dynamic_controls/longitudinal_control.h
+++ Core/Inc/dynamic_controls/longitudinal_control.h
@@ -12,25 +12,32 @@
 
 #include <stdint.h>
 
+//Defines the longitudinal control's sample time and setpoint
 #define LONGITUDINAL_DELAY 30
+//TODO (Guilherme): Pode ser interessante pensar em alterar o valor do setpoint. Durante o processo de validação viu-se que o valor ótimo de slip varia entre 0,1 e 0,2. Nesse sentido, pode ser interessante tentar definir qual seria o valor do slip ratio ideal do nosso pneu.
 #define IDEAL_SLIP_DRY     13
 #define IDEAL_SLIP_WET     30
 
-// defines PID LONGITUDINAL
-#define KP_LONGITUDINAL 6.51864262048678 // TODO(renanmoreira): check values
+//Defines the longitudinal PID's parameters
+//TODO (Guilherme): Ainda precisamos definir os parâmetros do controlador.
+#define KP_LONGITUDINAL 6.51864262048678 
 #define KI_LONGITUDINAL 0.14843404179579
 #define TI_LONGITUDINAL (KP_LONGITUDINAL / KI_LONGITUDINAL)
 
+//Creates a struct that contains a "wheel identifier" and this wheel individual longitudinal PID
 typedef struct {
+    //TODO (Guilherme): Essa variável "ref_decrease" me parece inútil. Não achei nenhum local além desse onde ela foi utilizada. Já existe uma variável "torque_decrease" que armazenará o resultado dos controladores.
     double ref_decrease;
     uint8_t wheel;
     PID_t pid_longitudinal;
 } longitudinal_t;
 
+//Creates a struct that will be used to store the longitudinal PID's outputs
 typedef struct {
     double torque_decrease[2];
 } longitudinal_control_result_t;
 
+//Initializes some functions that will be developed in the file "longitudinal_control.c"
 longitudinal_control_result_t longitudinal_control();
 double wheel_control(uint8_t wheel_motor, SPEEDS_t speeds);
 void init_longitudinal_control();

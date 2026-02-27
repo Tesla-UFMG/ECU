8bd20d4104929e41c8d7961e3b6534fecd377d61
ozne32 <enzorcc.mf3rs@gmail.com>
2026-02-27 08:13:39 -0300

    torque_manager.h: Traducao comentarios

--- Core/Inc/torque_command/torque_manager.h
+++ Core/Inc/torque_command/torque_manager.h
@@ -9,18 +9,17 @@
 #define INC_TORQUE_MANAGER_H_
 
 #include "stdint.h"
-
-// todo: adicionar novo tipo de controle quando for implementada a integração dos dois
-// controles
+//todo: add new type of control when the integration of two
+//controls is implemented
 
 typedef enum { LONGITUDINAL = 2, LATERAL = 1, NO_CONTROL = 0 } E_CONTROL_TYPE;
 
 E_CONTROL_TYPE g_control_type = NO_CONTROL;
 
-// defines rampa de torque
-#define INC_TORQUE         300 // inclinacao 2a parte da rampa
-#define INC_TORQUE_INIT    200 // inclinacao 1a parte
-#define TORQUE_INIT_LIMITE 500 // ponto de transicao entre as rampas
+// defines the torque ramp
+#define INC_TORQUE         300 // inclination of the second part of the ramp
+#define INC_TORQUE_INIT    200 // inclination of the first part
+#define TORQUE_INIT_LIMITE 500 // transition point of the ramps
 
 #define RAMPA_DELAY 30
 

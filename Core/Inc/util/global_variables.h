aa747675d20827e93769d7eaccaf23cddb56f7cf
ozne32 <enzorcc.mf3rs@gmail.com>
2026-02-27 08:23:01 -0300

    global_variables.h: Traducao comentarios

--- Core/Inc/util/global_variables.h
+++ Core/Inc/util/global_variables.h
@@ -10,13 +10,13 @@
 
 #include "global_definitions.h"
 
-// buffer atualizado pelo DMA com dados de leitura do ADC
+// updated buffer by the DMA with data read from the ADC
 extern volatile uint16_t ADC_DMA_buffer[ADC_LINES];
 
-// variavel de controle para desabilitar referencia de torque
+//control variable for disable the torque reference
 extern volatile uint8_t g_should_disable_engines;
 
-// variavel que guarda parametros do estado atual do veiculo
+// variable that holds the parameters of the vehicle's current state
 extern volatile vehicle_state_parameters_t g_vehicle_state_parameters;
 
 extern volatile const modos aceleracao;
@@ -25,7 +25,7 @@ extern volatile const modos autox;
 extern volatile const modos enduro;
 extern volatile const modos erro;
 
-// guarda o estado atual do veiculo (acelera, neutro, freia)
+// stores the vehicle's current state (accelerate, neutral, brake)
 extern volatile vehicle_state_e vehicle_state;
 
 #endif /* INC_GLOBAL_VARIABLES_H_ */

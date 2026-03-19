48fe20c9f0260896ad41d93bcc134cce09a97fc7
Guilherme Oliveira Porto de Paula <guilhermeoppaula@gmail.com>
2026-03-18 23:03:43 -0300

    longitudinal_control.c e .h: Implementando um tratamento mais intuitivo para o funcionamento adequado do controle longitudinal.

--- Core/Inc/dynamic_controls/longitudinal_control.h
+++ Core/Inc/dynamic_controls/longitudinal_control.h
@@ -17,17 +17,8 @@
 #define LONGITUDINAL_DELAY 30
 
 //TODO (Guilherme): Pode ser interessante pensar em alterar o valor do setpoint. Durante o processo de validação viu-se que o valor ótimo de slip varia entre 0,1 e 0,2. Nesse sentido, pode ser interessante tentar definir qual seria o valor do slip ratio ideal do nosso pneu.
-//All the slip related variables are defined with a negative sign. This is essential to make the code work correctly and in the most simple way possible. 
-/* 
-First it is important to understand that the idea of the longitudinal control is to reduce the torque of the motor when the slip is bigger than the ideal. 
-Therefore, we expect the controller to act when the slip is bigger than the ideal, that is, when the error (defined as "ideal slip - real slip") is negative.
-The problem is that if the error is negative the PID output will be negative as well, fact that would make the torque reference increase instead of decrease.
-
-The solution for this problem is to define the slip and the setpoint with a negative sign, so when the absolute value of the slip is bigger than the ideal, the error will be positive and the PID output will be negative.
-With this, the code will behave as expected, reducing the torque reference when the slip is bigger than the ideal and not changing it when the slip is smaller than the ideal.
-*/
-#define IDEAL_SLIP_DRY     -13
-#define IDEAL_SLIP_WET     -30
+#define IDEAL_SLIP_DRY     13
+#define IDEAL_SLIP_WET     30
 
 //Defines the longitudinal PID's parameters
 //TODO (Guilherme): Ainda precisamos definir os parâmetros do controlador.
--- Core/Src/dynamic_controls/longitudinal_control.c
+++ Core/Src/dynamic_controls/longitudinal_control.c
@@ -25,12 +25,13 @@ void init_longitudinal_control() {
 
     //Initializes the rear left PID
     PID_init(&pid_longitudinal, 1, KP_LONGITUDINAL,
-             TI_LONGITUDINAL, 0, NOMINAL_TORQUE, 0, LONGITUDINAL_DELAY);
+             TI_LONGITUDINAL, 0, 0, -NOMINAL_TORQUE, LONGITUDINAL_DELAY);
     //Defines the rear left PID's setpoint
     PID_set_setpoint(&pid_longitudinal, IDEAL_SLIP_DRY);
 }
 
-//Function that returns the result of an individual longitudinal controller. First it calculates the controller input and than it calls the "PID_compute" function (that gives us the controller's output)
+//Definition of the function that returns the result of an individual longitudinal controller. 
+//First it calculates the controller input and than it calls the "PID_compute" function (that gives us the controller's output).
 double wheel_control() {
     //Calculus variables
     double cg_speed;
@@ -45,13 +46,13 @@ double wheel_control() {
     if (cg_speed < 1) {
         slip = 0;
     } else {
-        //slip ratio calculation (the explanation of the negative sign is in the "longitudinal_control.h" file, in the definition of the setpoint). 
-        slip = -((rear_avg_speed - cg_speed)
+        //slip ratio calculation
+        slip = ((rear_avg_speed - cg_speed)
                 / cg_speed)
                * 100;
     }
 
-    return (double)(PID_compute(&(pid_longitudinal), slip));
+    return fabs((double)(PID_compute(&(pid_longitudinal), slip)));
 }
 
 

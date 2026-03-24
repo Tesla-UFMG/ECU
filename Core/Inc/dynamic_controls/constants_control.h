be560c0f06eea97bea532545fb5670012b48e044
JoaoGabrielAbreuMegali <jgmegali@gmail.com>
2026-03-24 16:59:02 -0300

    constants_control.h: atualizando as variáveis com os valores do nosso carro

--- Core/Inc/dynamic_controls/constants_control.h
+++ Core/Inc/dynamic_controls/constants_control.h
@@ -13,13 +13,13 @@
 #define FRONT_DISTANCE       1.0     	// distance between the CG and the front axle
 #define REAR_DISTANCE        0.549      // distance between the CG and the rear axle
 #define MASS                 412        // vehicle (342kg) + driver (70kg)
-#define FRONT_CORNER_STIFF   50420.286  // cornering stiffness of the front axle
-#define REAR_CORNER_STIFF    50420.286  // cornering stiffness of the rear axle
+#define FRONT_CORNER_STIFF   49870      // cornering stiffness of the front axle
+#define REAR_CORNER_STIFF    55370      // cornering stiffness of the rear axle
 #define FRICTION_COEFFICIENT 1.3
 #define TUNABILITY_FACTOR    0.8
 #define GRAVITY              9.81
 #define WHEELBASE            (FRONT_DISTANCE + REAR_DISTANCE)
-#define KU                                                                               \
+#define KU                                                                              \
    ( ( (REAR_DISTANCE * MASS) / (FRONT_CORNER_STIFF * WHEELBASE) )                       \
    - ( (FRONT_DISTANCE * MASS) / (REAR_CORNER_STIFF * WHEELBASE) ) )
 

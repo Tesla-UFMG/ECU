f1409209b82001806410bcbffe11b3ebbbcd81fc
Enzo Ribeiro de Castro Costa <enzorcc@ufmg.com.br>
2026-05-17 11:35:03 -0300

    RTD.h: Corrigindo o prototipo das variáveis

--- Core/Inc/driver_settings/RTD.h
+++ Core/Inc/driver_settings/RTD.h
@@ -11,7 +11,13 @@
 // FSAE Rules: EV.10.5.2.a (2023)
 // Note : Judge specifically requested to use maximum time during Electrical Inspection
 #define RTDS_TIME_MS 3000
+#include "cmsis_os.h"
+#include "util/util.h"
+
 
 void exit_RTD();
+void activate_RTDS();
+bool can_RTD_be_enabled();
+void set_RTD();
 
 #endif /* INC_RTD_H_ */

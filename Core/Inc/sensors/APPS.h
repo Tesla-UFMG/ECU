54528b78a9e9e47d7bb007a12629e7217e40f3d9
Leticia <leticiafleandro7@gmail.com>
2026-06-11 22:43:09 -0300

    Novos defines APPS

--- Core/Inc/sensors/APPS.h
+++ Core/Inc/sensors/APPS.h
@@ -9,45 +9,35 @@
 #define INC_THROTTLE_H_
 
 #include "stdint.h"
-#include "stdbool.h"
-
-// Initialization of functions
-typedef struct {
-    uint16_t deadzone_lower_limit;
-    uint16_t deadzone_upper_limit;
-    float adjust_parameters_slope;
-    float adjust_parameters_intercept;
-} apps_ref;
-
 
 #define THROTTLE_DELAY   25
-#define BRAKE_ACTIVE     1800 // value send by SU-F: 2800
-#define SU_F_ERROR       3200 // value send by SU-F: 3500
+#define BRAKE_ACTIVE     1500 // valor enviado pela SU-F: 2800
+#define SU_F_ERROR       2900 // valor enviado pela SU-F: 3500
 #define APPS_25_PERCENT  250
 #define APPS_05_PERCENT  50
-#define SU_F_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulation: T.4.3.3 (2022)
-#define APPS_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulation: T.4.2.5 (2022)
+#define SU_F_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.3.3 (2022)
+#define APPS_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.2.5 (2022)
 
-// Values bellow obtained during the APPS calibration
-// Explanations and more details available in the spreadsheet:
+// Valores abaixo obtidos durante a calibracao do APPS
+// Explicacoes e mais detalhes disponiveis na planilha:
 // https://docs.google.com/spreadsheets/d/1oN8rV0IG5MuoPPMHFQrklaCpWQLI31Kn/edit?usp=sharing&ouid=111087741893616112745&rtpof=true&sd=true
 
 #define APPS_MATRIX_LENGTH 2
 
 // APPS 1
-#define APPS1_LOWER_DEADZONE   370
-#define APPS1_UPPER_DEADZONE   1530
-#define APPS1_ADJUST_SLOPE     0.7843
-#define APPS1_ADJUST_INTERCEPT (-270.58)
-#define APPS1_MIN              345
-#define APPS1_MAX              1620
+#define APPS1_LOWER_DEADZONE   255
+#define APPS1_UPPER_DEADZONE   1055
+#define APPS1_ADJUST_SLOPE     1.25
+#define APPS1_ADJUST_INTERCEPT (-300.0)
+#define APPS1_MIN              240
+#define APPS1_MAX              1035
 
 // APPS 2
-#define APPS2_LOWER_DEADZONE   500
-#define APPS2_UPPER_DEADZONE   2100
-#define APPS2_ADJUST_SLOPE     0.5714
-#define APPS2_ADJUST_INTERCEPT (-268.57)
-#define APPS2_MIN              470
-#define APPS2_MAX              2220
+#define APPS2_LOWER_DEADZONE   505
+#define APPS2_UPPER_DEADZONE   2140
+#define APPS2_ADJUST_SLOPE     0.61
+#define APPS2_ADJUST_INTERCEPT (-300.31)
+#define APPS2_MIN              490
+#define APPS2_MAX              2120
 
 #endif /* INC_THROTTLE_H_ */

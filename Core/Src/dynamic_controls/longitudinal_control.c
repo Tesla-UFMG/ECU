32c7db59966dda528206dc214c940930d9c09715
Enzo Ribeiro de Castro Costa <enzorcc@ufmg.com.br>
2026-07-01 17:49:54 -0300

    longitudinal_control.c: tirando o log_data daqui, agora ele está em speed.c (manda independente se o controle estiver ligado ou desligado)

--- Core/Src/dynamic_controls/longitudinal_control.c
+++ Core/Src/dynamic_controls/longitudinal_control.c
@@ -64,8 +64,8 @@ longitudinal_control_result_t longitudinal_control() {
                 / cg_speed)
                * 100;
     }
-    double slip_data = (slip+1)*100;
-    log_data(ID_SLIP, slip_data);
+    // double slip_data = (slip+1)*100;
+    // log_data(ID_SLIP, slip_data);
     //This "if" is here to deactivate the controller when the slip is lower than the ideal or when the car is making a turn 
     if (slip <= 13 || internal_wheel != CENTRO) {
         pid_result = 0;

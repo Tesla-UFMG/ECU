96e7b62efdfd185cdbc15ee8f7382c1f3869be40
Enzo Ribeiro de Castro Costa <enzorcc@ufmg.com.br>
2026-07-15 17:35:01 -0300

    `APPS.h`: Passando o regulamento para 2026

--- Core/Inc/sensors/APPS.h
+++ Core/Inc/sensors/APPS.h
@@ -15,8 +15,8 @@
 #define SU_F_ERROR       2900 // valor enviado pela SU-F: 3500
 #define APPS_25_PERCENT  250
 #define APPS_05_PERCENT  50
-#define SU_F_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.3.3 (2022)
-#define APPS_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.2.5 (2022)
+#define SU_F_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.3.3 (2026)
+#define APPS_ERROR_TIMER (100 - THROTTLE_DELAY) // Regulamento: T.4.2.5 (2026)
 
 // Valores abaixo obtidos durante a calibracao do APPS
 // Explicacoes e mais detalhes disponiveis na planilha:
@@ -25,19 +25,19 @@
 #define APPS_MATRIX_LENGTH 2
 
 // APPS 1
-#define APPS1_LOWER_DEADZONE   255
+#define APPS1_LOWER_DEADZONE   250
 #define APPS1_UPPER_DEADZONE   1055
 #define APPS1_ADJUST_SLOPE     1.25
 #define APPS1_ADJUST_INTERCEPT (-300.0)
-#define APPS1_MIN              240
+#define APPS1_MIN              230
 #define APPS1_MAX              1035
 
 // APPS 2
-#define APPS2_LOWER_DEADZONE   505
+#define APPS2_LOWER_DEADZONE   500
 #define APPS2_UPPER_DEADZONE   2140
 #define APPS2_ADJUST_SLOPE     0.61
 #define APPS2_ADJUST_INTERCEPT (-300.31)
-#define APPS2_MIN              490
+#define APPS2_MIN              480
 #define APPS2_MAX              2120
 
 #endif /* INC_THROTTLE_H_ */
